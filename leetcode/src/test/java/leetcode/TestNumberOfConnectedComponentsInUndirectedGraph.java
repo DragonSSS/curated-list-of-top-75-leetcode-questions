aa9c29aa210c7a25dee74acf62f6a6ce1f960eef
DragonSSS <songweilong1989@gmail.com>
2020-08-26 23:11:08 -0400

    - Add test case first

--- leetcode/src/test/java/leetcode/TestNumberOfConnectedComponentsInUndirectedGraph.java
+++ leetcode/src/test/java/leetcode/TestNumberOfConnectedComponentsInUndirectedGraph.java
@@ -2,10 +2,19 @@ package leetcode;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestNumberOfConnectedComponentsInUndirectedGraph {
 
     @Test
     public void testCountComponents() {
+        int n1 = 5;
+        int[][] edeges1 = new int[][]{{0,1}, {1,2}, {3,4}};
 
+        int n2 = 5;
+        int[][] edeges2 = new int[][]{{0,1}, {1,2}, {2,3}, {3,4}};
+
+        assertEquals(2, new  NumberOfConnectedComponentsInUndirectedGraph().countComponents(n1, edeges1));
+        assertEquals(1, new NumberOfConnectedComponentsInUndirectedGraph().countComponents(n2, edeges2));
     }
 }
