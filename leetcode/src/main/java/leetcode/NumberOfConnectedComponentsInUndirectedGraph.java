2225058fd2099cbe9823a64ae4561130ac572c84
DragonSSS <songweilong1989@gmail.com>
2020-08-31 21:07:41 -0400

    - Add impl of leetcode 323

--- leetcode/src/main/java/leetcode/NumberOfConnectedComponentsInUndirectedGraph.java
+++ leetcode/src/main/java/leetcode/NumberOfConnectedComponentsInUndirectedGraph.java
@@ -1,10 +1,49 @@
 package leetcode;
 
+import java.util.*;
+
 public class NumberOfConnectedComponentsInUndirectedGraph {
     public int countComponents(int n, int[][] edges) {
 
+        if (n == 0 || edges == null || edges.length == 0)
+            return 0;
 
-        int res = 0;
-        return res;
+        if (n == 1)
+            return n;
+
+        Map<Integer, ArrayList<Integer>> graph = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            graph.put(i, new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        Set<Integer> visited = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited.contains(i)) {
+                count++;
+                queue.offer(i);
+
+                while (!queue.isEmpty()) {
+                    int cur = queue.poll();
+                    visited.add(cur);
+
+                    for(int next : graph.get(cur)) {
+                        if (!visited.contains(next)) {
+                            queue.offer(next);
+                        }
+                    }
+                }
+            }
+        }
+
+        return  count;
     }
 }
