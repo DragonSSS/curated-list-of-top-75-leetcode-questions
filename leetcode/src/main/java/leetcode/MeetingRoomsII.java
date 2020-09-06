f29d8cf6de80c7a43000701446a08780f00bf7bc
DragonSSS <songweilong1989@gmail.com>
2020-09-05 23:52:32 -0400

    - Add meeting rooms II

---
+++ leetcode/src/main/java/leetcode/MeetingRoomsII.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class MeetingRoomsII {
+    public int minMeetingRooms(int[][] intervals) {
+        return 0;
+    }
+}
---
+++ leetcode/src/test/java/leetcode/TestMeetingRoomsII.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+import org.junit.Test;
+
+public class TestMeetingRoomsII {
+
+    @Test
+    public void testMinMeetingRooms() {
+
+    }
+}
