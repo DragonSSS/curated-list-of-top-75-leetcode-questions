27b15c3e62caaae50ce30182e7c5f24023f08c9f
DragonSSS <songweilong1989@gmail.com>
2020-09-06 23:55:34 -0400

    - Add impl of meeting rooms II

--- leetcode/src/main/java/leetcode/MeetingRoomsII.java
+++ leetcode/src/main/java/leetcode/MeetingRoomsII.java
@@ -1,7 +1,25 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 public class MeetingRoomsII {
     public int minMeetingRooms(int[][] intervals) {
-        return 0;
+        if (intervals == null || intervals.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        heap.add(intervals[0][1]);
+
+        for(int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= heap.peek()) {
+                heap.poll();
+            }
+            heap.add(intervals[i][1]);
+        }
+
+        return heap.size();
     }
 }
