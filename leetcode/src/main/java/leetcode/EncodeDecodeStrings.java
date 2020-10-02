5f1ede54c6a0658ffab85e6d89cda680fa747024
DragonSSS <songweilong1989@gmail.com>
2020-10-01 21:49:07 -0400

    - Add EncodeDecodeStrings

---
+++ leetcode/src/main/java/leetcode/EncodeDecodeStrings.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import java.util.List;
+
+public class EncodeDecodeStrings {
+    public String encode(List<String> strs) {
+        return null;
+    }
+
+    public List<String> decode(String s) {
+        return null;
+    }
+}
