474a7d7b8d0ae37a9b6844bfbe55deb36854b36e
DragonSSS <songweilong1989@gmail.com>
2020-10-03 22:58:17 -0400

    - Add Invert tree and find Pythogorean triplet

---
+++ dailycodingprogram/src/main/java/dailycodingproblem/google/InvertBinaryTree.java
@@ -0,0 +1,30 @@
+package dailycodingproblem.google;
+
+import util.TreeNode;
+
+/*
+This problem was asked by Google.
+
+Invert a binary tree.
+
+For example, given the following tree:
+
+    a
+   / \
+  b   c
+ / \  /
+d   e f
+should become:
+
+  a
+ / \
+ c  b
+ \  / \
+  f e  d
+*/
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        return root;
+    }
+}
