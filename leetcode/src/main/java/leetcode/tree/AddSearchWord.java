c9b5ca370c122ae433b922f1d971c5590892aedd
DragonSSS <songweilong1989@gmail.com>
2020-11-10 02:48:23 -0500

    - Design Add and Search Words Data Structure

--- leetcode/src/main/java/leetcode/tree/AddSearchWord.java
+++ leetcode/src/main/java/leetcode/tree/AddSearchWord.java
@@ -3,19 +3,55 @@ package leetcode.tree;
 public class AddSearchWord {
     class WordDictionary {
 
+        class TreeNode {
+            public TreeNode[] children;
+            public boolean isWord;
+
+            TreeNode() {
+                this.children = new TreeNode[26];
+                this.isWord = false;
+            }
+        }
+
+        TreeNode root;
         /** Initialize your data structure here. */
         public WordDictionary() {
-
+            this.root = new TreeNode();
         }
 
         /** Adds a word into the data structure. */
         public void addWord(String word) {
+            char[] chars = word.toCharArray();
+            TreeNode node = root;
+            for (char c : chars) {
+                if (node.children[c - 'a'] == null)
+                    node.children[c - 'a'] = new TreeNode();
+                node = node.children[c - 'a'];
+            }
+            node.isWord = true;
 
         }
 
         /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
         public boolean search(String word) {
-            return false;
+            return helper(word, 0, root);
+        }
+
+        private  boolean helper(String word, int index, TreeNode node) {
+            if (index == word.length())
+                return node.isWord;
+
+            char c = word.charAt(index);
+
+            if (c == '.') {
+                for (int i = 0; i < 26; i++) {
+                    if (node.children[i] != null && helper(word, index + 1, node.children[i]))
+                        return true;
+                }
+                return false;
+            } else {
+                return node.children[c - 'a'] != null && helper(word, index + 1, node.children[c - 'a']);
+            }
         }
     }
 
