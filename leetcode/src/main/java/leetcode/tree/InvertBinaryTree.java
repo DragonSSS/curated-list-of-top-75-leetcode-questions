7355d02a579a58d89540b403025010fb0030a731
DragonSSS <songweilong1989@gmail.com>
2020-11-19 21:28:07 -0500

    - Impl InvertBinaryTree

--- leetcode/src/main/java/leetcode/tree/InvertBinaryTree.java
+++ leetcode/src/main/java/leetcode/tree/InvertBinaryTree.java
@@ -2,8 +2,30 @@ package leetcode.tree;
 
 import util.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class InvertBinaryTree {
     public TreeNode invertTree(TreeNode root) {
-        return null;
+        if (root == null)
+            return root;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            TreeNode temp = cur.left;
+            cur.left = cur.right;
+            cur.right = temp;
+
+            if (cur.left != null)
+                queue.offer(cur.left);
+
+            if (cur.right != null)
+                queue.offer(cur.right);
+        }
+
+        return root;
     }
 }
