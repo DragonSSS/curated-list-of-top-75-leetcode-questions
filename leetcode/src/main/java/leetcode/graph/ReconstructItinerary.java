c558b604c432fe91fa5e87d96258a54ac48693e9
DragonSSS <songweilong1989@gmail.com>
2020-12-01 13:24:13 -0500

    - Reconstruct Itinerary

---
+++ leetcode/src/main/java/leetcode/graph/ReconstructItinerary.java
@@ -0,0 +1,9 @@
+package leetcode.graph;
+
+import java.util.List;
+
+public class ReconstructItinerary {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        return null;
+    }
+}
