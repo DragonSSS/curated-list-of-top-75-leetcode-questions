a7986a20593f6a0902f4a86659d5acb65b272c0b
DragonSSS <songweilong1989@gmail.com>
2020-12-10 16:55:03 -0500

    - Imple clone graph using dfs

--- leetcode/src/main/java/leetcode/graph/CloneGraph.java
+++ leetcode/src/main/java/leetcode/graph/CloneGraph.java
@@ -2,8 +2,29 @@ package leetcode.graph;
 
 import util.Node;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CloneGraph {
+    // dfs
     public Node cloneGraph(Node node) {
-        return null;
+        if (node == null)
+            return node;
+        Map<Integer, Node> map = new HashMap<>();
+        return helper(node, map);
+    }
+
+
+    private Node helper(Node node, Map<Integer, Node> map) {
+        if (map.containsKey(node.val)) {
+            return map.get(node.val);
+        } else {
+            map.put(node.val, new Node(node.val));
+            for (Node n : node.neighbors) {
+                map.get(node.val).neighbors.add(helper(n, map));
+            }
+        }
+
+        return map.get(node.val);
     }
 }
