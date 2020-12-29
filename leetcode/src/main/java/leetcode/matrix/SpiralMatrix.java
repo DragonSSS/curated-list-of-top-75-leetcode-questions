4289b4cbd6180de4360870755785a2135fd8ca83
DragonSSS <songweilong1989@gmail.com>
2020-12-29 10:02:18 -0500

    - Update SpiralMatrix

--- leetcode/src/main/java/leetcode/matrix/SpiralMatrix.java
+++ leetcode/src/main/java/leetcode/matrix/SpiralMatrix.java
@@ -6,6 +6,36 @@ import java.util.List;
 public class SpiralMatrix {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> res = new ArrayList<>();
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int top = 0;
+        int bottom = row - 1;
+
+        int left = 0;
+        int right = col - 1;
+
+        while(res.size() < row * col) {
+            for(int i = left; i <= right && res.size() < row * col; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+
+            for(int i = top; i <= bottom && res.size() < row * col; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+
+            for(int i = right; i >= left && res.size() < row * col; i--) {
+                res.add(matrix[bottom][i]);
+            }
+            bottom--;
+
+            for(int i = bottom; i <= top && res.size() < row * col; i++) {
+                res.add(matrix[left][i]);
+            }
+            left++;
+        }
         return res;
     }
 }
