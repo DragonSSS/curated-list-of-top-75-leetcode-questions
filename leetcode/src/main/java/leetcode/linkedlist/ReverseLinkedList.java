0dd32bf8864a96083ebb1f01c30fad97f8c0043d
DragonSSS <songweilong1989@gmail.com>
2020-12-30 00:03:00 -0500

    - reverse linked list

--- leetcode/src/main/java/leetcode/linkedlist/ReverseLinkedList.java
+++ leetcode/src/main/java/leetcode/linkedlist/ReverseLinkedList.java
@@ -4,6 +4,29 @@ import util.ListNode;
 
 public class ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
-        return null;
+        // return helper(head, null);
+        if (head == null)
+            return head;
+
+        ListNode pre = null;
+
+        while(head != null) {
+            ListNode temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+        }
+
+        return pre;
+    }
+
+    // recursion
+    private ListNode helper(ListNode node, ListNode pre) {
+        if (node == null)
+            return pre;
+
+        ListNode temp = node.next;
+        node.next = pre;
+        return helper(temp, node);
     }
 }
