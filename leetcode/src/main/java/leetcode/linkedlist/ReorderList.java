4cef5edbc88e3fb2dd8d9762b7e4c442c7e10680
DragonSSS <songweilong1989@gmail.com>
2021-01-01 16:27:04 -0500

    - Update

---
+++ leetcode/src/main/java/leetcode/linkedlist/ReorderList.java
@@ -0,0 +1,22 @@
+package leetcode.linkedlist;
+
+import util.ListNode;
+
+public class ReorderList {
+    // 1-2-3-4
+    // 0-1-2-3
+    // 1-4-2-3
+    // 0-3-1-2
+
+    // 1-2-3-4-5
+    // 1-5-2-4-3
+
+    // find the mid to split two linked lists
+    // reverse second one
+    // merge two linked list into one
+    public void reorderList(ListNode head) {
+
+    }
+}
+
+
