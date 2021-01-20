5d18c551dce355f8ff3efefcd5b5583ec11dcc46
DragonSSS <songweilong1989@gmail.com>
2021-01-19 22:19:46 -0500

    - Imple MissingNumber

--- leetcode/src/main/java/leetcode/binary/MissingNumber.java
+++ leetcode/src/main/java/leetcode/binary/MissingNumber.java
@@ -3,8 +3,16 @@ package leetcode.binary;
 public class MissingNumber {
     // n , [0, n] and missing one value;
     // [0, 1, 3] missing 2
+    // index: 0, 1, 2
 
+    // [0, 1]
+    // index: 0, 1
+    // missing 2
     public int missingNumber(int[] nums) {
-        return 0;
+        int res = nums.length;
+        for(int i = 0; i < nums.length; i++) {
+            res = res ^ i ^ nums[i];
+        }
+        return res;
     }
 }
