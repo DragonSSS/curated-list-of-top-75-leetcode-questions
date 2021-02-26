c3f8b91bdc65c349b7d1e4a630ac5695e3efa7fb
DragonSSS <songweilong1989@gmail.com>
2021-02-25 23:44:28 -0500

    - Update NonOverlappingIntervals

--- leetcode/src/main/java/leetcode/interval/NonOverlappingIntervals.java
+++ leetcode/src/main/java/leetcode/interval/NonOverlappingIntervals.java
@@ -21,4 +21,25 @@ public class NonOverlappingIntervals {
 
         return intervals.length - count;
     }
+
+    // The other version
+    /*
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if(intervals == null || intervals.length == 0)
+            return 0;
+        Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);
+        int preEnd = intervals[0][1];
+        int count = 0;
+
+        for(int i = 1; i < intervals.length; i++) {
+            if(intervals[i][0] >= preEnd) {
+                preEnd = intervals[i][1];
+            } else {
+                count++;
+                preEnd = Math.min(preEnd, intervals[i][1]);
+            }
+        }
+
+        return count;
+    }*/
 }
