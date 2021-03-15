1cb4977c1b6c383c66ca30060e1c0aa02d90d51a
DragonSSS <songweilong1989@gmail.com>
2021-03-14 23:20:30 -0400

    - Update

--- leetcode/src/main/java/leetcode/array/FindMinimumInRotatedSortedArray.java
+++ leetcode/src/main/java/leetcode/array/FindMinimumInRotatedSortedArray.java
@@ -2,15 +2,17 @@ package leetcode.array;
 
 public class FindMinimumInRotatedSortedArray {
     public int findMin(int[] nums) {
-        if (nums.length == 0)
-            return 0;
+        // edge case
+        int len = nums.length;
+        if (nums[0] <= nums[len - 1])
+            return nums[0];
 
-        // 3 4 5 1 2
         int start = 0;
-        int end = nums.length - 1;
+        int end = len - 1;
         while(start < end) {
             int mid = start + (end - start) / 2;
-            if(nums[mid] > nums[end]) {
+
+            if (nums[mid] > nums[end]) {
                 start = mid + 1;
             } else {
                 end = mid;
