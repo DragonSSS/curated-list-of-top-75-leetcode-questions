d6f18e9b19d09e14ca0011d2b1a474e759e4409a
DragonSSS <songweilong1989@gmail.com>
2021-03-26 20:22:54 -0400

    - Update

--- leetcode/src/main/java/leetcode/dp/WordBreak.java
+++ leetcode/src/main/java/leetcode/dp/WordBreak.java
@@ -1,23 +1,23 @@
 package leetcode.dp;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class WordBreak {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return helper(0, s, new HashSet<>(wordDict), new boolean[s.length() + 1]);
+        boolean[] visited = new boolean[s.length() + 1];
+        return helper(s, 0, wordDict, visited);
     }
 
-    private boolean helper(int index, String s, Set<String> dict, boolean[] visited) {
+    private boolean helper(String s, int index, List<String> wordDict, boolean[] visited) {
         if (index == s.length())
             return true;
 
         visited[index] = true;
 
+        // note:  i < s.length() + 1 to get all of chars as substring()
         for(int i = index + 1; i < s.length() + 1; i++) {
-            String cur = s.substring(index, i);
-            if(!visited[i] && dict.contains(cur) && helper(i, s, dict, visited))
+            String subStr = s.substring(index, i);
+            if (!visited[i] && wordDict.contains(subStr) && helper(s, i, wordDict, visited))
                 return true;
         }
 
