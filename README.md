ab44a18b55a894321391c4cd635a237157d0c0b5
DragonSSS <songweilong1989@gmail.com>
2021-04-30 00:11:33 -0400

    - Imple Vertical Order Traversal of a Binary Tree

--- README.md
+++ README.md
@@ -646,9 +646,8 @@
   - [solution](https://github.com/DragonSSS/curated-list-of-top-75-leetcode-questions/blob/master/leetcode/src/main/java/leetcode/tree/AllNodesDistanceKInBinaryTree.java)
   - hint: convert tree to graph via hashmap, avoid duplicates via hashset, bfs with queue or dfs with recursion
 
-- [ ] LeetCode #987 Vertical Order Traversal of a Binary Tree (Tree)
+- [x] LeetCode #987 Vertical Order Traversal of a Binary Tree (Tree)
   - [https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/)
   - [solution](https://github.com/DragonSSS/curated-list-of-top-75-leetcode-questions/blob/master/leetcode/src/main/java/leetcode/tree/VerticalOrderTraversalOfBinaryTree.java)
-  - hint:
-  
+  - hint: similar to  LeetCode #314, but use dfs (vertical and horizontal params) + `TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map`
 -->
--- leetcode/src/main/java/leetcode/tree/VerticalOrderTraversalOfBinaryTree.java
+++ leetcode/src/main/java/leetcode/tree/VerticalOrderTraversalOfBinaryTree.java
@@ -2,10 +2,43 @@ package leetcode.tree;
 
 import util.TreeNode;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 public class VerticalOrderTraversalOfBinaryTree {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        return null;
+        List<List<Integer>> res = new ArrayList<>();
+
+        // edge case
+        if(root == null)
+            return res;
+
+        Map<Integer, Map<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        helper(root, 0, 0 , map);
+
+        for(Map<Integer, PriorityQueue<Integer>> subMap : map.values()) {
+            res.add(new ArrayList<>());
+            for(PriorityQueue<Integer> pq : subMap.values()) {
+                while(!pq.isEmpty()) {
+                    res.get(res.size() - 1).add(pq.poll());
+                }
+            }
+        }
+        return res;
+    }
+
+    private void helper(TreeNode node, int hor, int ver, Map<Integer, Map<Integer, PriorityQueue<Integer>>> map) {
+        if (node == null)
+            return;
+
+        map.putIfAbsent(hor, new TreeMap<>());
+        map.get(hor).putIfAbsent(ver, new PriorityQueue<>());
+        map.get(hor).get(ver).offer(node.val);
+
+        helper(node.left, hor - 1, ver + 1, map);
+        helper(node.right, hor + 1, ver + 1, map);
     }
 }
