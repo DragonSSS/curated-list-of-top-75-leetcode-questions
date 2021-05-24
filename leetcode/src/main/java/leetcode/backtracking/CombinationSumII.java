8512e3bbb4046a79543e998c83cc6c79e0d790e9
DragonSSS <songweilong1989@gmail.com>
2021-05-23 23:42:11 -0400

    - Update

--- leetcode/src/main/java/leetcode/backtracking/CombinationSumII.java
+++ leetcode/src/main/java/leetcode/backtracking/CombinationSumII.java
@@ -6,9 +6,29 @@ import java.util.List;
 
 public class CombinationSumII {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        // O(nlogn)
         Arrays.sort(candidates);
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> cur = new ArrayList<>();
+
+        helper(0, candidates, target, cur, res);
         return  res;
+    }
+
+    private void helper(int index, int[]candidates,  int target, List<Integer> cur, List<List<Integer>> res) {
+        if(target < 0)
+            return;
+
+        if(target == 0) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+
+        for(int i = index; i < candidates.length; i++) {
+            if(i > index && candidates[i] == candidates[i - 1]) continue;
+            cur.add(candidates[i]);
+            helper(i + 1, candidates, target - candidates[i], cur, res);
+            cur.remove(cur.size() - 1);
+        }
     }
 }
