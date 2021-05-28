8e5223b4e4b8f163ba43ef4976391c16c737dc0b
DragonSSS <songweilong1989@gmail.com>
2021-05-27 20:46:10 -0400

    - Update

---
+++ leetcode/src/main/java/leetcode/heap/KthSmallestElementInSortedMatrix.java
@@ -0,0 +1,21 @@
+package leetcode.heap;
+
+import java.util.PriorityQueue;
+
+public class KthSmallestElementInSortedMatrix {
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                pq.offer(matrix[i][j]);
+            }
+        }
+
+        int res = 0;
+        while(k-- > 0) {
+            res = pq.poll();
+        }
+
+        return res;
+    }
+}
