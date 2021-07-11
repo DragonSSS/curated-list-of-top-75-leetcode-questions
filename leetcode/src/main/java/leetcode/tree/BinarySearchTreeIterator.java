6ba8900ff297d40b5519548181cfff308e8097bf
DragonSSS <songweilong1989@gmail.com>
2021-07-10 23:30:44 -0400

    - Update

--- leetcode/src/main/java/leetcode/tree/BinarySearchTreeIterator.java
+++ leetcode/src/main/java/leetcode/tree/BinarySearchTreeIterator.java
@@ -2,17 +2,30 @@ package leetcode.tree;
 
 import util.TreeNode;
 
+import java.util.Stack;
+
 public class BinarySearchTreeIterator {
+    Stack<TreeNode> stack;
 
     public BinarySearchTreeIterator(TreeNode root) {
-
+        stack = new Stack<>();
+        add(root);
     }
 
     public int next() {
-        return -1;
+        TreeNode node = stack.pop();
+        add(node.right);
+        return node.val;
     }
 
     public boolean hasNext() {
-        return false;
+        return !stack.isEmpty();
+    }
+
+    private void add(TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
     }
 }
