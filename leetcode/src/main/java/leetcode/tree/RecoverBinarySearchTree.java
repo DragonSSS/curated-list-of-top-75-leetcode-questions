3bb94ac43b9341364e5a461debe4ebc5aba682c2
DragonSSS <songweilong1989@gmail.com>
2021-07-19 23:50:06 -0400

    - update

--- leetcode/src/main/java/leetcode/tree/RecoverBinarySearchTree.java
+++ leetcode/src/main/java/leetcode/tree/RecoverBinarySearchTree.java
@@ -3,7 +3,33 @@ package leetcode.tree;
 import util.TreeNode;
 
 public class RecoverBinarySearchTree {
+    TreeNode first = null;
+    TreeNode second = null;
+    TreeNode previous = new TreeNode(Integer.MIN_VALUE);
     public void recoverTree(TreeNode root) {
+        find(root);
 
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void find(TreeNode node) {
+        if (node == null)
+            return;
+
+        find(node.left);
+
+        if (first == null && previous.val > node.val) {
+            first = previous;
+        }
+
+        if (first != null && previous.val > node.val) {
+            second = node;
+        }
+
+        previous = node;
+
+        find(node.right);
     }
 }
