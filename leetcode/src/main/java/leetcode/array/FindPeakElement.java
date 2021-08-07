2cbc2e8ba55e7b78c8e87b453368486a4d7d0a53
DragonSSS <songweilong1989@gmail.com>
2021-08-06 20:52:16 -0400

    - update

--- leetcode/src/main/java/leetcode/array/FindPeakElement.java
+++ leetcode/src/main/java/leetcode/array/FindPeakElement.java
@@ -1,7 +1,32 @@
 package leetcode.array;
 
 public class FindPeakElement {
-    public int findPeakElement(int[] nums) {
-        return -1;
+    public int findPeakElement_linearScan(int[] nums) {
+        for(int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1])
+                return i;
+        }
+
+        return nums.length - 1;
+    }
+
+    public int findPeakElement_binarySearch(int[] nums) {
+        int n = nums.length;
+        int start = 0, end = n - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            // condition: nums[i] != nums[i + 1] for all valid i
+            // peak is at range [mid + 1, n - 1]
+            if (nums[mid] < nums[mid + 1]) {
+                start = mid + 1;
+            // peak is at range [0, mid]
+            } else {
+                end = mid;
+            }
+        }
+
+        return start;
     }
 }
