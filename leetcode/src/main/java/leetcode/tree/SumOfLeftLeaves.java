eea93641dffd133af71da87619a9f0bd40887f81
DragonSSS <songweilong1989@gmail.com>
2021-08-30 23:06:06 -0400

    - update

--- leetcode/src/main/java/leetcode/tree/SumOfLeftLeaves.java
+++ leetcode/src/main/java/leetcode/tree/SumOfLeftLeaves.java
@@ -4,6 +4,19 @@ import util.TreeNode;
 
 public class SumOfLeftLeaves {
     public int sumOfLeftLeaves(TreeNode root) {
-        return 0;
+        return helper(root, false);
+    }
+
+    private int helper (TreeNode node, boolean isLeft) {
+        if (node == null)
+            return 0;
+        if (node.left == null && node.right == null) {
+            if (isLeft)
+                return node.val;
+            else
+                return 0;
+        }
+
+        return helper(node.left, true) + helper(node.right, false);
     }
 }
