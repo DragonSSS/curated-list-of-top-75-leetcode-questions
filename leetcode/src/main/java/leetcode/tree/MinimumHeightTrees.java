9709017f891a55fd50b10027f852896c5ab30bfa
DragonSSS <songweilong1989@gmail.com>
2021-09-20 23:32:01 -0400

    - update

--- leetcode/src/main/java/leetcode/tree/MinimumHeightTrees.java
+++ leetcode/src/main/java/leetcode/tree/MinimumHeightTrees.java
@@ -1,9 +1,44 @@
 package leetcode.tree;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MinimumHeightTrees {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        return null;
+        if (n == 1)
+            return Collections.singletonList(0);
+
+        // build graph
+        List<Set<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new HashSet<>());
+        }
+
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        List<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (graph.get(i).size() == 1)
+                leaves.add(i);
+        }
+
+        while( n > 2) {
+            n -= leaves.size();
+            List<Integer> newLeaves = new ArrayList<>();
+            for (int i : leaves) {
+                int j = graph.get(i).iterator().next();
+                graph.get(j).remove(i);
+                if (graph.get(j).size() == 1)
+                    newLeaves.add(j);
+            }
+            leaves = newLeaves;
+        }
+        return leaves;
     }
 }
