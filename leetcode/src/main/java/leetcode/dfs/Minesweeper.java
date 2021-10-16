c039f160b1ac3d11610b348c8d1586b74363c275
DragonSSS <songweilong1989@gmail.com>
2021-10-15 23:58:19 -0400

    - update

--- leetcode/src/main/java/leetcode/dfs/Minesweeper.java
+++ leetcode/src/main/java/leetcode/dfs/Minesweeper.java
@@ -2,6 +2,11 @@ package leetcode.dfs;
 
 public class Minesweeper {
     public char[][] updateBoard(char[][] board, int[] click) {
-        return null;
+        int x = click[0], y = click[1];
+        if (board[x][y] == 'M') {
+            board[x][y] = 'X';
+            return board;
+        }
+        return board;
     }
 }
