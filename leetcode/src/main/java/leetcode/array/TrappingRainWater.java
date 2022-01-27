8081414831778658e7fd99b9e5c0317ed310cbd0
DragonSSS <songweilong1989@gmail.com>
2022-01-27 00:11:40 -0500

    - update

--- leetcode/src/main/java/leetcode/array/TrappingRainWater.java
+++ leetcode/src/main/java/leetcode/array/TrappingRainWater.java
@@ -1,7 +1,42 @@
 package leetcode.array;
 
 public class TrappingRainWater {
+
     public int trap(int[] height) {
+        if (height.length == 0)  
+            return 0;  
+          
+        int i, max, total = 0;
+        int left[] = new int[height.length];
+        int right[] = new int[height.length];  
+  
+        // from left to right
+        left[0] = height[0];
+        max = height[0];
+        for (i = 1; i < height.length; i++) {  
+            left[i] = Math.max(max, height[i]);
+            max = Math.max(max, height[i]);
+        }  
+  
+        // from right to left
+        right[height.length-1] = height[height.length-1];
+        max = height[height.length-1];
+        for (i = height.length-2; i >= 0; i--) {  
+            right[i] = Math.max(max, height[i]);
+            max = Math.max(max, height[i]);
+        }  
+  
+        // trapped water (when i==0 and i == height.length - 1, it cannot trapped any water)
+        for (i = 1; i < height.length-1; i++) {  
+            int bit = Math.min(left[i], right[i]) - height[i];  
+            if (bit > 0)  
+                total += bit;  
+        }  
+  
+        return total;  
+    }
+
+    public int trap_improve_version(int[] height) {
         // edge case
         if (height.length == 0)
             return 0;
