df1534677c78c0f627fecddd38475154191b9156
DragonSSS <songweilong1989@gmail.com>
2022-02-16 23:56:40 -0500

    - update

--- leetcode/src/main/java/leetcode/array/MaximumProductSubarray.java
+++ leetcode/src/main/java/leetcode/array/MaximumProductSubarray.java
@@ -18,4 +18,21 @@ public class MaximumProductSubarray {
 
         return res;
     }
+
+    public int maxProduct_2r(int[] nums) {
+        int[] minDp = new int[nums.length];
+        int[] maxDp = new int[nums.length];
+        
+        minDp[0] = nums[0];
+        maxDp[0] = nums[0];
+        int res = Integer.MIN_VALUE;
+        for(int i = 1; i < nums.length; i++) {
+           minDp[i] = Math.min(Math.min(minDp[i - 1] * nums[i], maxDp[i - 1] * nums[i]), nums[i]);
+           maxDp[i] = Math.max(Math.max(minDp[i - 1] * nums[i], maxDp[i - 1] * nums[i]), nums[i]);
+            res = Math.max(res, maxDp[i]);
+        }
+        res = Math.max(res, maxDp[0]);
+        
+        return res;
+    }
 }
