0ad6b0f793d52252ceb33672366e47f8113ef28f
DragonSSS <songweilong1989@gmail.com>
2022-02-17 22:57:39 -0500

    - update

--- leetcode/src/main/java/leetcode/array/FindMinimumInRotatedSortedArray.java
+++ leetcode/src/main/java/leetcode/array/FindMinimumInRotatedSortedArray.java
@@ -21,4 +21,29 @@ public class FindMinimumInRotatedSortedArray {
 
         return nums[start];
     }
+
+    public int findMin_2r(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        
+        int res = nums[0];
+        while(start <= end) {
+            
+            if (nums[start] <= nums[end]) {
+                res = Math.min(nums[start], res);
+                break;
+            }
+            
+            int mid = start + (end - start) / 2;
+            res = Math.min(nums[mid], res);
+            
+            if (nums[mid] >= nums[start]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return res;
+    }
 }
--- leetcode/src/main/java/leetcode/array/SearchInRotatedSortedArray.java
+++ leetcode/src/main/java/leetcode/array/SearchInRotatedSortedArray.java
@@ -33,4 +33,33 @@ public class SearchInRotatedSortedArray {
 
         return -1;
     }
+
+    public int search_2r(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if(nums[mid] == target)
+                return mid;
+            
+            //left sorted
+            if (nums[mid] >= nums[start]) {
+                if(target > nums[mid] || target < nums[start]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            // right sorted
+            } else {
+                if(target < nums[mid] || target > nums[end]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
 }
