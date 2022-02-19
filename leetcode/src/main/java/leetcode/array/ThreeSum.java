948a19513e610620af8104c23707616bc813dbbf
DragonSSS <songweilong1989@gmail.com>
2022-02-18 21:44:57 -0500

    - update

--- leetcode/src/main/java/leetcode/array/ThreeSum.java
+++ leetcode/src/main/java/leetcode/array/ThreeSum.java
@@ -44,4 +44,36 @@ public class ThreeSum {
 
         return res;
     }
+
+    public List<List<Integer>> threeSum_2r(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 2; i++) {
+            if(i >= 1 && nums[i] == nums[i-1])
+                continue;
+            
+            int start = i + 1;
+            int end = nums.length - 1;
+            while(start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[start], nums[end]));
+                    while(start < end && nums[start] == nums[start + 1]) {
+                        start++;
+                    }
+                    while(start < end && nums[end] == nums[end - 1]) {
+                        end--;
+                    }
+                    start++;
+                    end--;
+                } else if (sum < 0) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
+        return res;
+    }
 }
