c260f7e3ac844f7d6dc55c81d17c80a78740b997
DragonSSS <songweilong1989@gmail.com>
2022-02-20 09:39:46 -0500

    - update

--- leetcode/src/main/java/leetcode/binary/NumberOfOneBits.java
+++ leetcode/src/main/java/leetcode/binary/NumberOfOneBits.java
@@ -1,6 +1,7 @@
 package leetcode.binary;
 
 public class NumberOfOneBits {
+    // lte version
     public int hammingWeight(int n) {
         int res = 0;
         while(n != 0) {
@@ -8,6 +9,18 @@ public class NumberOfOneBits {
             n >>>= 1;
         }
 
+        return res;
+    }
+
+    public int hammingWeight_2r(int n) {
+        int res = 0;
+        int mask = 1;
+        for(int i = 0; i < 32; i++) {
+            if((mask & n) != 0)
+                res++;
+            mask = mask << 1;
+        }
+        
         return res;
     }
 }
