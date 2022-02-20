bb961311cc596d41567e6c68cd95238f52870820
DragonSSS <songweilong1989@gmail.com>
2022-02-20 16:00:57 -0500

    - update

--- leetcode/src/main/java/leetcode/binary/ReverseBits.java
+++ leetcode/src/main/java/leetcode/binary/ReverseBits.java
@@ -15,4 +15,21 @@ public class ReverseBits {
 
         return res;
     }
+
+    public int reverseBits_2r(int n) {
+        int res = 0;
+        if (n == 0)
+            return n;
+        
+        
+        for(int i = 0; i < 32; i++) {
+            res = res << 1;
+            if ((n & 1) == 1) {
+                res+=1;
+            }
+            n = n >> 1;
+        }
+        
+        return res;
+    }
 }
