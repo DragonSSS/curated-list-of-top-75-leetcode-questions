f665bbb02207a94ef425e846c1d2e52b414b5705
DragonSSS <songweilong1989@gmail.com>
2022-02-21 21:05:21 -0500

    - update

--- leetcode/src/main/java/leetcode/dp/LongestCommonSubsequence.java
+++ leetcode/src/main/java/leetcode/dp/LongestCommonSubsequence.java
@@ -3,6 +3,8 @@ package leetcode.dp;
 public class LongestCommonSubsequence {
     public int longestCommonSubsequence(String text1, String text2) {
         Integer[][] memo = new Integer[text1.length()][text2.length()];
+        // 2rd round func
+        // return helper_2r(text1, text2, 0, 0, memo);
         return helper(text1, text2, 0, 0, memo);
     }
 
@@ -24,6 +26,25 @@ public class LongestCommonSubsequence {
         int res = Math.max(res1, res2);
 
         memo[cur1][cur2] = res;
+        return res;
+    }
+
+    private int helper_2r(String text1, String text2, int index1, int index2, Integer[][] memo) {
+        if(index2 == text2.length() || index1 == text1.length())
+            return 0;
+        
+        if(memo[index1][index2] != null)
+            return memo[index1][index2];
+        
+        int curEqual = 0;
+        if (text1.charAt(index1) == text2.charAt(index2)) {
+            curEqual = helper(text1, text2, index1 + 1, index2 + 1, memo) + 1;
+        }
+        
+        int nonEqual = Math.max(helper(text1, text2, index1, index2 + 1, memo), helper(text1, text2, index1 + 1, index2, memo));
+        
+        int res = Math.max(curEqual, nonEqual);
+        memo[index1][index2] = res;
         return res;
     }
 }
