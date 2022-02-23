e5793cc1ca04cb63f2477929b523275abd30380f
DragonSSS <songweilong1989@gmail.com>
2022-02-22 23:12:38 -0500

    - update

--- leetcode/src/main/java/leetcode/dp/WordBreak.java
+++ leetcode/src/main/java/leetcode/dp/WordBreak.java
@@ -1,6 +1,10 @@
 package leetcode.dp;
 
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 
 public class WordBreak {
     public boolean wordBreak(String s, List<String> wordDict) {
@@ -21,6 +25,31 @@ public class WordBreak {
                 return true;
         }
 
+        return false;
+    }
+
+    public boolean wordBreak_2r_bfs(String s, List<String> wordDict) {
+        Set<String> words = new HashSet<>(wordDict);
+        Set<Integer> visited = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(0);
+        visited.add(0);
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                int cur = queue.poll();
+                if(cur == s.length())
+                    return true;
+                for(int j = cur + 1; j < s.length() + 1; j++) {
+                    String sub = s.substring(cur, j);
+                    if(words.contains(sub) && !visited.contains(j)) {
+                        queue.offer(j);
+                        visited.add(j);
+                    }
+                }
+            }
+        }
         return false;
     }
 }
