e01b68ca09247f4998d839441b0a7b7b32f795f9
DragonSSS <songweilong1989@gmail.com>
2022-02-25 19:45:45 -0500

    - update

--- leetcode/src/main/java/leetcode/dp/CombinationSumIV.java
+++ leetcode/src/main/java/leetcode/dp/CombinationSumIV.java
@@ -1,5 +1,10 @@
 package leetcode.dp;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
 public class CombinationSumIV {
     // top-down recursion + memorization
     public int combinationSum4(int[] nums, int target) {
@@ -21,6 +26,50 @@ public class CombinationSumIV {
             }
         }
         memo[target] = res;
+        return res;
+    }
+    
+    Map<Integer, Integer> memo = new HashMap<>();
+    public int combinationSum4_2r(int[] nums, int target) {
+        if (target == 0)
+            return 1;
+        
+        if (memo.containsKey(target))
+            return memo.get(target);
+        
+        int res = 0;
+        for(int num : nums) {
+            if(target - num >= 0) {
+                res += combinationSum4_2r(nums, target - num);
+            }
+        }
+        
+        memo.put(target, res);
+        return res;
+    }
+
+    public int combinationSum4_2r_bfs_tle(int[] nums, int target) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(target);
+        
+        int res = 0;
+        
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int cur = queue.poll();
+                if (cur == 0) {
+                    res++;
+                    continue;
+                }
+                for(int num : nums) {
+                    if (cur - num >= 0) {
+                        queue.offer(cur - num);
+                    }
+                }
+            }
+        }
+        
         return res;
     }
 }
