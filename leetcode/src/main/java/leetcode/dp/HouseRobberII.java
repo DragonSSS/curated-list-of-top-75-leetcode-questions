3c6ab6ae6f5e7c4605c6a69755513a4d85595f93
DragonSSS <songweilong1989@gmail.com>
2022-02-27 10:34:41 -0500

    - update

--- leetcode/src/main/java/leetcode/dp/HouseRobberII.java
+++ leetcode/src/main/java/leetcode/dp/HouseRobberII.java
@@ -29,4 +29,46 @@ public class HouseRobberII {
         memo[cur] = res;
         return res;
     }
+
+    Integer[] memo1;
+    Integer[] memo2;
+    public int rob_2r(int[] nums) {
+        memo1 = new Integer[nums.length];
+        memo2 = new Integer[nums.length];
+        
+        if(nums.length == 1)
+            return nums[0];
+        
+        int robFirst = helper_2r(nums, 0, true);
+        int robSecond = helper_2r(nums, 1, false);
+        
+        return Math.max(robFirst, robSecond);
+    }
+    
+    private int helper_2r(int[] nums, int index, boolean robFirst) {
+        if(robFirst && index >= nums.length - 1) {
+            return 0;
+        } else if (!robFirst && index >= nums.length) {
+            return 0;
+        }
+        
+        if(robFirst && memo1[index] != null ) {
+            return memo1[index];
+        } else if (!robFirst && memo2[index] != null) {
+            return memo2[index];
+        }
+        
+        int skip = helper_2r(nums, index + 1, robFirst);
+        int nonSkip = helper_2r(nums, index + 2, robFirst) + nums[index];
+        
+        int res = Math.max(skip, nonSkip);
+
+        if (robFirst) {
+            memo1[index] = res;
+        } else {
+            memo2[index] = res;
+        }
+        
+        return res;
+    }
 }
