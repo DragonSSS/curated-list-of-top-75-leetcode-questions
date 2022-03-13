dc98533d74467f60168790f4864043afcbf9276b
DragonSSS <songweilong1989@gmail.com>
2022-03-13 09:20:22 -0400

    - update

--- leetcode/src/main/java/leetcode/graph/LongestConsecutiveSequence.java
+++ leetcode/src/main/java/leetcode/graph/LongestConsecutiveSequence.java
@@ -1,6 +1,8 @@
 package leetcode.graph;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestConsecutiveSequence {
     public int longestConsecutive(int[] nums) {
@@ -26,5 +28,31 @@ public class LongestConsecutiveSequence {
         }
 
         return res;
+    }
+
+    Set<Integer> numsSet = new HashSet<>();
+    Set<Integer> visited = new HashSet<>();
+    public int longestConsecutive_sr_dfs(int[] nums) {
+        if(nums.length == 0)
+            return 0;
+        
+        for(int num : nums) {
+            numsSet.add(num);
+        }
+        
+        int res = 0;
+        for(int num : nums) {
+            res = Math.max(res, helper_2r_dfs(num));
+        }
+        return res;
+    }
+    
+    private int helper_2r_dfs(int num) {
+        if(!numsSet.contains(num) || visited.contains(num))
+            return 0;
+        
+        visited.add(num);
+        int curLen = helper_2r_dfs(num - 1) + 1 + helper_2r_dfs(num + 1);
+        return curLen;
     }
 }
