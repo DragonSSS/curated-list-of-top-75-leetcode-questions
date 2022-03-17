58327e937fb4f52b096949be5b6c1820975d2a62
DragonSSS <songweilong1989@gmail.com>
2022-03-16 23:59:40 -0400

    - update

--- leetcode/src/main/java/leetcode/graph/AlienDictionary.java
+++ leetcode/src/main/java/leetcode/graph/AlienDictionary.java
@@ -2,10 +2,12 @@ package leetcode.graph;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Set;
 
 public class AlienDictionary {
     // Kahn's algorithm or dfs approach
@@ -92,6 +94,86 @@ public class AlienDictionary {
                 return true;
         }
         visited[c - 'a'] = false;
+        return false;
+    }
+
+    Map<Character, List<Character>> graph = new HashMap<>();
+    int[] indegree = new int[26];
+    public String alienOrder_2r(String[] words) {        
+        for(String word : words) {
+            for(char c : word.toCharArray()) {
+                graph.putIfAbsent(c, new ArrayList<>());
+            }
+        }
+        
+        // build graph
+        for(int i = 1; i < words.length; i++) {
+            String word1 = words[i - 1];
+            String word2 = words[i];
+            
+            int minLen = Math.min(word1.length(), word2.length());
+            // invalid case
+            if(word1.length() > word2.length() && word1.startsWith(word2))
+                return "";
+            
+            for(int j = 0; j < minLen; j++) {
+                char c1 = word1.charAt(j);
+                char c2 = word2.charAt(j);
+                if(c1 != c2) {
+                    if (!graph.get(c1).contains(c2)) {
+                        graph.get(c1).add(c2);
+                        indegree[c2 - 'a']++;
+                    }
+                    break;
+                }
+            }
+        }
+
+        // check DAG, make sure there is no circle
+        for(char c : graph.keySet()) {
+            if(circleExist_2r(c, graph, new HashSet<>())) {
+                return "";
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        // bfs
+        Queue<Character> queue = new LinkedList<>();
+        for(char c : graph.keySet()) {
+            if(indegree[c - 'a'] == 0) {
+                queue.offer(c);
+            }
+        }
+        
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                char cur = queue.poll();
+                sb.append(cur);
+                for(char next : graph.get(cur)) {
+                    indegree[next - 'a']--;
+                    if (indegree[next - 'a'] == 0) {
+                        queue.offer(next);
+                    }
+                }
+            }
+        }
+        
+        return sb.toString();
+    }
+    
+    // dfs + backtracking
+    private boolean circleExist_2r(char c, Map<Character, List<Character>> graph, Set<Character> visited)
+    {
+        if(visited.contains(c))
+            return true;
+        
+        visited.add(c);
+        for(char next : graph.get(c)) {
+            if(circleExist_2r(next, graph, visited))
+                return true;
+        }
+        visited.remove(c);
         return false;
     }
 }
