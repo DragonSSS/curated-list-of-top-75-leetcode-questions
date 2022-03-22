53f6cde1623bcbc262a1034ab4045cf2a72218bb
DragonSSS <songweilong1989@gmail.com>
2022-03-21 23:08:32 -0400

    - update

--- leetcode/src/main/java/leetcode/interval/MeetingRooms.java
+++ leetcode/src/main/java/leetcode/interval/MeetingRooms.java
@@ -17,4 +17,20 @@ public class MeetingRooms {
 
         return true;
     }
+
+    public boolean canAttendMeetings_2r(int[][] intervals) {
+        if (intervals.length <= 1)
+            return true;
+        Arrays.sort(intervals, (a, b) -> a[0] - b [0]);
+        int preEnd = intervals[0][1];
+        for(int i = 1; i < intervals.length; i++) {
+            int curStart = intervals[i][0];
+            if (preEnd > curStart) {
+                return false;
+            }
+            preEnd = intervals[i][1];
+        }
+        
+        return true;
+    }
 }
