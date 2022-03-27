abe42fb1efaa2b22047fff22f102fdc6e2505c95
DragonSSS <songweilong1989@gmail.com>
2022-03-27 10:51:04 -0400

    - update

--- leetcode/src/main/java/leetcode/matrix/SetMatrixZeroes.java
+++ leetcode/src/main/java/leetcode/matrix/SetMatrixZeroes.java
@@ -22,4 +22,26 @@ public class SetMatrixZeroes {
             }
         }
     }
+
+    public void setZeroes_2r(int[][] matrix) {
+        boolean[] row = new boolean[matrix.length];
+        boolean[] col = new boolean[matrix[0].length];
+        
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j< matrix[0].length; j++) {
+                if(matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+        
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j< matrix[0].length; j++) {
+                if(row[i] || col [j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        } 
+    }
 }
