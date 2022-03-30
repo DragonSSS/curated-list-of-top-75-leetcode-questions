8dd18fcdfc610f93bee851b5c4260ca69aa3c0ee
DragonSSS <songweilong1989@gmail.com>
2022-03-29 21:13:45 -0400

    - update

--- leetcode/src/main/java/leetcode/matrix/RotateImage.java
+++ leetcode/src/main/java/leetcode/matrix/RotateImage.java
@@ -19,4 +19,22 @@ public class RotateImage {
             }
         }
     }
+
+    public void rotate_2r(int[][] matrix) {
+        int m = matrix.length - 1;
+        int n = matrix[0].length - 1;
+        for(int i = 0; i <=  n / 2; i++) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[n - i];
+            matrix[n - i] = temp;
+        }
+        
+        for(int i = 0; i <= m; i++) {
+            for(int j = i; j <= n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
 }
