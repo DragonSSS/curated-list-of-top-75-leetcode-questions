5005e48be64e60b641bfe6e17e488fabd301c674
DragonSSS <songweilong1989@gmail.com>
2022-04-07 23:48:31 -0400

    - update

--- leetcode/src/main/java/leetcode/string/LongestPalindromicSubstring.java
+++ leetcode/src/main/java/leetcode/string/LongestPalindromicSubstring.java
@@ -26,4 +26,33 @@ public class LongestPalindromicSubstring {
 
         return s.substring(i + 1, j);
     }
+
+    public String longestPalindrome_2r(String s) {
+        if(s.length()  == 1)
+            return s;
+        
+        String res = s.substring(0, 1);
+        
+        for(int i = 0; i < s.length(); i++) {
+            String odd = helper_2r(i, i, s);
+            if (odd.length() > res.length()) {
+                res = odd;
+            }
+            
+            String even = helper_2r(i, i + 1, s);
+            if (even.length() > res.length()) {
+                res = even;
+            }
+        }
+        return res;
+    }
+    
+    private String helper_2r(int left, int right, String s) {
+        while(left >= 0 && right <= s.length() - 1 && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        
+        return s.substring(left + 1, right);
+    }
 }
