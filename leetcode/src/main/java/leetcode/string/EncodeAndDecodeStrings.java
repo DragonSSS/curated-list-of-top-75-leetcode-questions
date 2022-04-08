2e4f44b3873bf558d80324e996e71c2c768a7215
DragonSSS <songweilong1989@gmail.com>
2022-04-08 00:25:21 -0400

    - update

--- leetcode/src/main/java/leetcode/string/EncodeAndDecodeStrings.java
+++ leetcode/src/main/java/leetcode/string/EncodeAndDecodeStrings.java
@@ -3,40 +3,6 @@ package leetcode.string;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
-Design an algorithm to encode a list of strings to a string.
-The encoded string is then sent over the network and is decoded back to the original list of strings.
-
-Machine 1 (sender) has the function:
-
-string encode(vector<string> strs) {
-  // ... your code
-  return encoded_string;
-}
-
-Machine 2 (receiver) has the function:
-
-vector<string> decode(string s) {
-  //... your code
-  return strs;
-}
-
-So Machine 1 does:
-string encoded_string = encode(strs);
-
-and Machine 2 does:
-vector<string> strs2 = decode(encoded_string);
-
-strs2 in Machine 2 should be the same as strs in Machine 1.
-
-Implement the encode and decode methods.
-
-Note:
-The string may contain any possible characters out of 256 valid ascii characters.
-Your algorithm should be generalized enough to work on any possible characters.
-Do not use class member/global/static variables to store states. Your encode and decode algorithms should be stateless.
-Do not rely on any library method such as eval or serialize methods. You should implement your own encode/decode algorithm.
-*/
 public class EncodeAndDecodeStrings {
     public String encode(List<String> strs) {
         if (strs.size() == 0)
@@ -66,4 +32,32 @@ public class EncodeAndDecodeStrings {
 
         return res;
     }
+
+        // Encodes a list of strings to a single string.
+        public String encode_2r(List<String> strs) {
+            if(strs.size() == 0)
+                return null;
+            StringBuilder sb = new StringBuilder();
+            for(String s : strs) {
+                sb.append(s.length()).append('/').append(s);
+            }
+            return sb.toString();
+        }
+    
+        // Decodes a single string to a list of strings.
+        public List<String> decode_2r(String s) {
+            List<String> res = new ArrayList<>();
+            if (s == null)
+                return res;
+            
+            int index = 0;
+            while(index < s.length()) {
+                int slash = s.indexOf('/', index);
+                int size = Integer.valueOf(s.substring(index, slash));
+                res.add(s.substring(slash + 1, slash + size + 1));
+                index = slash + size + 1;
+            }
+            
+            return res;
+        }
 }
