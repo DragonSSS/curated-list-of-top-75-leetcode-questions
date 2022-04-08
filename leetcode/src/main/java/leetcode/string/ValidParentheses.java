cf848025e3deddbb12f94f1c19e91c32fe9dd7b9
DragonSSS <songweilong1989@gmail.com>
2022-04-07 23:23:40 -0400

    - update

--- leetcode/src/main/java/leetcode/string/ValidParentheses.java
+++ leetcode/src/main/java/leetcode/string/ValidParentheses.java
@@ -26,4 +26,25 @@ public class ValidParentheses {
 
         return stack.isEmpty();
     }
+
+    public boolean isValid_2r(String s) {
+        Stack<Character> stack = new Stack<>();    
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (!stack.isEmpty()) {
+                if(c == ')' && stack.pop() != '(') {
+                    return false;
+                } else if(c == '}' && stack.pop() != '{') {
+                    return false;
+                } else if(c == ']' && stack.pop() != '[') {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
 }
