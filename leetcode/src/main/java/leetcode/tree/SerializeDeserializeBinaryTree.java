f254dda02d322406031ae204512df8a4d816141c
DragonSSS <songweilong1989@gmail.com>
2022-04-10 13:51:21 -0400

    - update

--- leetcode/src/main/java/leetcode/tree/SerializeDeserializeBinaryTree.java
+++ leetcode/src/main/java/leetcode/tree/SerializeDeserializeBinaryTree.java
@@ -4,6 +4,7 @@ import util.TreeNode;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class SerializeDeserializeBinaryTree {
     // Encodes a tree to a single string.
@@ -53,5 +54,52 @@ public class SerializeDeserializeBinaryTree {
         root.right = deserializeHelper(list);
 
         return root;
+    }
+
+        // Encodes a tree to a single string.
+    public String serialize_2r(TreeNode root) {
+        if (root == null)
+            return null;
+        
+        StringBuilder sb = new StringBuilder();
+        serializeHelper_2r(root, sb);
+        
+        return sb.substring(0, sb.length() - 1);
+    }
+    
+    private void serializeHelper_2r(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append("null");
+            sb.append(",");
+            return;
+        }
+        
+        sb.append(String.valueOf(node.val));
+        sb.append(",");
+        serializeHelper(node.left, sb);
+        serializeHelper(node.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize_2r(String data) {
+        if (data == null)
+            return null;
+        
+        String[] nodes = data.split(",");
+        Queue<String> queue = new LinkedList<>(Arrays.asList(nodes));
+        TreeNode root = deserializeHelper_2r(queue);
+        return root;
+    }
+    
+    private TreeNode deserializeHelper_2r(Queue<String> queue) {
+        String cur = queue.poll();
+        if (cur.equals("null")) {
+            return null;
+        }
+        
+        TreeNode node = new TreeNode(Integer.valueOf(cur));
+        node.left = deserializeHelper_2r(queue);
+        node.right = deserializeHelper_2r(queue);
+        return node;
     }
 }
