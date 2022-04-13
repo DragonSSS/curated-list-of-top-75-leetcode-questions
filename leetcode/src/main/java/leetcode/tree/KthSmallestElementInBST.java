21a7c878c8afe5a828933c324b89e6fbac70f817
DragonSSS <songweilong1989@gmail.com>
2022-04-13 00:05:27 -0400

    - update

--- leetcode/src/main/java/leetcode/tree/KthSmallestElementInBST.java
+++ leetcode/src/main/java/leetcode/tree/KthSmallestElementInBST.java
@@ -20,4 +20,26 @@ public class KthSmallestElementInBST {
         list.add(node.val);
         helper(node.right, list);
     }
+
+    // inorder traversal + dfs
+    int res = 0;
+    public int kthSmallest_2r(TreeNode root, int k) {
+        helper(root, k);
+        return res;
+    }
+    
+    private int helper(TreeNode node, int k) {
+        if (node == null) {
+            return k;
+        }
+        
+        k = helper(node.left, k);
+        k--;
+        if (k == 0) {
+            res = node.val;
+            return k;
+        }
+        k = helper(node.right, k);
+        return k;
+    }
 }
