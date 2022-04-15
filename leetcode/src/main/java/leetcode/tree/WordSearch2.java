59dfa6432d061606ddc412af5a23ee990ff8c8e1
DragonSSS <songweilong1989@gmail.com>
2022-04-15 10:10:43 -0400

    - update

--- leetcode/src/main/java/leetcode/tree/WordSearch2.java
+++ leetcode/src/main/java/leetcode/tree/WordSearch2.java
@@ -15,7 +15,6 @@ public class WordSearch2 {
         }
     }
 
-    // memorization
     boolean[][] visited;
 
     public List<String> findWords(char[][] board, String[] words) {
@@ -71,5 +70,68 @@ public class WordSearch2 {
         dfs(board, cur, i, j + 1, res);
         dfs(board, cur, i, j - 1, res);
         visited[i][j] = false;
+    }
+
+    // class TreeNode {
+    //     public TreeNode[] children;
+    //     public String word;
+        
+    //     public TreeNode() {
+    //         this.children = new TreeNode[26];
+    //         this.word = null;
+    //     }
+    // }
+    
+    // boolean[][] visited;
+    TreeNode root;
+    int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    public List<String> findWords_2r(char[][] board, String[] words) {
+        List<String> res = new ArrayList<>();
+        root = new TreeNode();
+        visited = new boolean[board.length][board[0].length];
+        
+        buildTrie_2r(words);
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                char cur = board[i][j];
+                if (root.children[cur - 'a'] != null) {
+                    helper_2r(i, j, root, board, res);
+                }
+            }
+        }
+        
+        return res;
+    }
+    
+    private void helper_2r(int i, int j, TreeNode node, char[][] board, List<String> res) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || node.children[board[i][j] - 'a'] == null)
+            return;
+        
+        TreeNode next = node.children[board[i][j] - 'a'];
+        if (next.word != null) {
+            res.add(next.word);
+            // remove duplicates
+            next.word = null;
+        }
+        
+        visited[i][j] = true;
+        for(int[] dir : dirs) {
+            helper_2r(i + dir[0], j + dir[1], next, board, res);
+        }
+        visited[i][j] = false;
+    }
+    
+    private void buildTrie_2r(String[] words) {
+        for(String word : words) {
+            TreeNode node = root;
+            char[] chars = word.toCharArray();
+            for(char c : chars) {
+                if (node.children[c - 'a'] == null) {
+                    node.children[c - 'a'] = new TreeNode();
+                }
+                node = node.children[c - 'a'];
+            }
+            node.word = word;
+        }
     }
 }
