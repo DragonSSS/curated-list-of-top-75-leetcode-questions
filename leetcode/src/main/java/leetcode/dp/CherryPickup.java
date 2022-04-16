40bd0b45ac64a6686879f8612c64aecd495f6a61
DragonSSS <songweilong1989@gmail.com>
2022-04-16 11:28:40 -0400

    - update

--- leetcode/src/main/java/leetcode/dp/CherryPickup.java
+++ leetcode/src/main/java/leetcode/dp/CherryPickup.java
@@ -37,4 +37,38 @@ public class CherryPickup {
         memo.put(key, res);
         return res;
     }
+
+    Map<String, Integer> memo = new HashMap<>();
+    public int cherryPickup_2r(int[][] grid) {
+        return Math.max(0, helper_2r(0, 0, 0, 0, grid));
+    }
+    
+    private int helper_2r(int i, int j, int m, int n, int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        
+        if (i >= row || j >= col || m >= row || n >= col || grid[i][j] == -1 || grid[m][n] == -1)
+            return Integer.MIN_VALUE;
+        
+        String key = i + "_" + j + "_" + m + "_" + n;
+        
+        if (memo.containsKey(key))
+            return memo.get(key);
+        
+        if (i == row - 1 && j == col - 1)
+            return grid[i][j];
+        
+        if (m == row - 1 && n == col - 1)
+            return grid[m][n];
+        
+        int curRes = i == m && j == n? grid[i][j] : grid[i][j] + grid[m][n];
+        
+        curRes += Math.max(
+            Math.max(helper_2r(i + 1, j, m + 1, n, grid), helper_2r(i, j + 1, m, n + 1, grid)),
+            Math.max(helper_2r(i + 1, j, m, n + 1, grid), helper_2r(i, j + 1, m + 1, n, grid))
+        );
+        
+        memo.put(key, curRes);
+        return curRes;
+    }
 }
