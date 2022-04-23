384be40d7ea087df0ff5b8e9555257bb125b8f7b
DragonSSS <songweilong1989@gmail.com>
2022-04-23 19:44:36 -0400

    - update comment

--- leetcode/src/main/java/leetcode/array/MaximumLengthOfSubarrayWithPositiveProduct.java
+++ leetcode/src/main/java/leetcode/array/MaximumLengthOfSubarrayWithPositiveProduct.java
@@ -8,6 +8,7 @@ public class MaximumLengthOfSubarrayWithPositiveProduct {
         int totalNegative = 0;
 
         for (int i = 0; i < nums.length; i++) {
+            // if num is negative, we update total negative and record first negative index
             if (nums[i] < 0) {
                 totalNegative++;
                 if (negativeIndex == -1) {
@@ -15,14 +16,17 @@ public class MaximumLengthOfSubarrayWithPositiveProduct {
                 }
             } 
             
+            // recourd zero index and reset negativeIndex and totalNegative
             if (nums[i] == 0) {
                 zeroIndex = i;
                 negativeIndex = -1;
                 totalNegative = 0;
             } else {
+                // we only care latest zero index position
                 if (totalNegative % 2 == 0) {
                     max = Math.max(max, i - zeroIndex);
                 } else {
+                    // exclude current negative index, only care first negative index
                     max = Math.max(max, i - negativeIndex);
                 }
             }
