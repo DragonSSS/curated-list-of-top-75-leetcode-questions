44ff9f06c082f60e8f62c8f431ccd6eb60d68588
DragonSSS <songweilong1989@gmail.com>
2022-04-26 23:04:35 -0400

    - update

--- leetcode/src/main/java/leetcode/array/TwoSum.java
+++ leetcode/src/main/java/leetcode/array/TwoSum.java
@@ -1,6 +1,7 @@
 package leetcode.array;
 
 import java.util.HashMap;
+import java.util.Map;
 
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
@@ -13,5 +14,19 @@ class TwoSum {
             map.put(target - nums[i], i);
         }
         return null;
+    }
+
+    public int[] twoSum_2r(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int rest = target - nums[i];
+            if (map.containsKey(rest)) {
+                return new int[]{i, map.get(rest)};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        
+        return new int[]{};
     }
 }
