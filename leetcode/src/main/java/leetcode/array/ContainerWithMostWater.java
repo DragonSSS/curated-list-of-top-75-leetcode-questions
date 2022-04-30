c77a918a92321a4cd108311ab3e92645598be329
DragonSSS <songweilong1989@gmail.com>
2022-04-29 23:06:20 -0400

    - update

--- leetcode/src/main/java/leetcode/array/ContainerWithMostWater.java
+++ leetcode/src/main/java/leetcode/array/ContainerWithMostWater.java
@@ -36,4 +36,18 @@ public class ContainerWithMostWater {
         
         return res;
     }
+
+    public int maxArea_3r(int[] height) {
+        int left = 0, right = height.length - 1;
+        int res = 0;
+        while(left < right) {
+            res = Math.max(res, (right - left) * Math.min(height[left], height[right]));
+            if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return res;
+    }
 }
