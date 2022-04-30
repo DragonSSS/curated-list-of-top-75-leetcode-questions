112945fe4b84d7e7621d2af5d73641d2ad2b310a
DragonSSS <songweilong1989@gmail.com>
2022-04-29 20:22:17 -0400

    - update

--- leetcode/src/main/java/leetcode/array/ThreeSum.java
+++ leetcode/src/main/java/leetcode/array/ThreeSum.java
@@ -76,4 +76,41 @@ public class ThreeSum {
         }
         return res;
     }
+
+    public List<List<Integer>> threeSum_3r(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums.length < 3) {
+            return res;
+        }
+        
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            if (i >= 1 && nums[i] == nums[i - 1])
+                continue;
+            
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while(left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while(left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        
+        return res;
+    }
 }
