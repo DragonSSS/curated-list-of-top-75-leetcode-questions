5b8b3e68bb1e2ea461a68fad5c2ece0dfcd640ef
DragonSSS <songweilong1989@gmail.com>
2022-05-01 19:36:30 -0400

    - update

--- leetcode/src/main/java/leetcode/dp/ClimbingStairs.java
+++ leetcode/src/main/java/leetcode/dp/ClimbingStairs.java
@@ -98,4 +98,23 @@ public class ClimbingStairs {
         memo[n] = res;
         return res;
     }
+
+    public int climbStairs_3r(int n) {
+        if (n == 1)
+            return 1;
+        
+        if (n == 2)
+            return 2;
+        
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
+    }
 }
