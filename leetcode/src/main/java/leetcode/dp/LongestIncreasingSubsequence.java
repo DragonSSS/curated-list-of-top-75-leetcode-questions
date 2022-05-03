e3e90d84bfa14ffb1779a185ad2a3f10e0210e84
DragonSSS <songweilong1989@gmail.com>
2022-05-02 23:58:56 -0400

    - update

--- leetcode/src/main/java/leetcode/dp/LongestIncreasingSubsequence.java
+++ leetcode/src/main/java/leetcode/dp/LongestIncreasingSubsequence.java
@@ -24,4 +24,29 @@ public class LongestIncreasingSubsequence {
         memo[pre + 1][cur] = res;
         return res;
     }
+
+    Integer[][] memo;
+    public int lengthOfLIS_2r(int[] nums) {
+        memo = new Integer[nums.length + 1][nums.length];
+        return helper_2r(0, -1, nums);
+    }
+    
+    private int helper_2r(int index, int pre, int[] nums) {
+        if (index == nums.length)
+            return 0;
+        
+        if(memo[pre + 1][index] != null)
+            return memo[pre + 1][index];
+        
+        int with = 0;
+        if (pre == -1 || nums[index] > nums[pre]) {
+            with = helper_2r(index + 1, index, nums) + 1;
+        }
+        
+        int without = helper_2r(index + 1, pre, nums);
+        
+        int res = Math.max(with, without);
+        memo[pre + 1][index] = res;
+        return res;
+    }
 }
