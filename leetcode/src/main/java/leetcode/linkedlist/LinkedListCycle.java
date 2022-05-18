baa81f9c8bc3008bef9b66a1bd1c293385560e59
DragonSSS <songweilong1989@gmail.com>
2022-05-17 20:13:00 -0400

    - update

--- leetcode/src/main/java/leetcode/linkedlist/LinkedListCycle.java
+++ leetcode/src/main/java/leetcode/linkedlist/LinkedListCycle.java
@@ -37,4 +37,18 @@ public class LinkedListCycle {
         
         return false;
     }
+
+    public boolean hasCycle_3r(ListNode head) {
+        ListNode slow = head, fast = head;
+        
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (slow == fast)
+                return true;
+        }
+        
+        return false;
+    }
 }
