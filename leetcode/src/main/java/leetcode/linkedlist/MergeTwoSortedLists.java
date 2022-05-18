be528ba4fec21e67ae6dfa4e7732f8b10c8e4903
DragonSSS <songweilong1989@gmail.com>
2022-05-17 20:17:25 -0400

    - update

--- leetcode/src/main/java/leetcode/linkedlist/MergeTwoSortedLists.java
+++ leetcode/src/main/java/leetcode/linkedlist/MergeTwoSortedLists.java
@@ -62,4 +62,30 @@ public class MergeTwoSortedLists {
         
         return dummy.next;
     }
+
+    public ListNode mergeTwoLists_3r(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode node = dummy;
+        
+        while(list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+                node.next = list1;
+                list1 = list1.next;
+            } else {
+                node.next = list2;
+                list2 = list2.next;
+            }
+            node = node.next;
+        }
+        
+        if (list1 != null) {
+            node.next = list1;
+        }
+        
+        if (list2 != null) {
+            node.next = list2;
+        }
+        
+        return dummy.next;
+    }
 }
