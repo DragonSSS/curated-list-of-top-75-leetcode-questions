14a42be59ecbed9784ff663ec9698e85599d0237
DragonSSS <songweilong1989@gmail.com>
2022-05-18 23:49:05 -0400

    - update

--- leetcode/src/main/java/leetcode/string/LongestRepeatingCharacterReplacement.java
+++ leetcode/src/main/java/leetcode/string/LongestRepeatingCharacterReplacement.java
@@ -47,4 +47,27 @@ public class LongestRepeatingCharacterReplacement {
         }
         return res;
     }
+
+    public int characterReplacement_3r(String s, int k) {
+        int[] freq = new int[26];
+        int left = 0;
+        int right = 0;
+        int res = 0;
+        int maxFreq = 0;
+        while(right < s.length()) {
+            char c = s.charAt(right);
+            freq[c - 'A']++;
+            maxFreq = Math.max(maxFreq, freq[c - 'A']);
+            int curLen = right - left + 1;
+            if (curLen - maxFreq > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            } else {
+                res = Math.max(res, curLen);
+            }
+            right++;
+        }
+        
+        return res;
+    }
 }
