eace46d0bdd1e81671328c4616f8e5c99df05e38
DragonSSS <songweilong1989@gmail.com>
2022-05-20 07:27:15 -0400

    - update

--- leetcode/src/main/java/leetcode/string/EncodeAndDecodeStrings.java
+++ leetcode/src/main/java/leetcode/string/EncodeAndDecodeStrings.java
@@ -33,31 +33,60 @@ public class EncodeAndDecodeStrings {
         return res;
     }
 
-        // Encodes a list of strings to a single string.
-        public String encode_2r(List<String> strs) {
-            if(strs.size() == 0)
-                return null;
-            StringBuilder sb = new StringBuilder();
-            for(String s : strs) {
-                sb.append(s.length()).append('/').append(s);
-            }
-            return sb.toString();
+    // Encodes a list of strings to a single string.
+    public String encode_2r(List<String> strs) {
+        if(strs.size() == 0)
+            return null;
+        StringBuilder sb = new StringBuilder();
+        for(String s : strs) {
+            sb.append(s.length()).append('/').append(s);
         }
+        return sb.toString();
+    }
     
-        // Decodes a single string to a list of strings.
-        public List<String> decode_2r(String s) {
-            List<String> res = new ArrayList<>();
-            if (s == null)
-                return res;
-            
-            int index = 0;
-            while(index < s.length()) {
-                int slash = s.indexOf('/', index);
-                int size = Integer.valueOf(s.substring(index, slash));
-                res.add(s.substring(slash + 1, slash + size + 1));
-                index = slash + size + 1;
-            }
-            
+    // Decodes a single string to a list of strings.
+    public List<String> decode_2r(String s) {
+        List<String> res = new ArrayList<>();
+        if (s == null)
             return res;
+            
+        int index = 0;
+        while(index < s.length()) {
+            int slash = s.indexOf('/', index);
+            int size = Integer.valueOf(s.substring(index, slash));
+            res.add(s.substring(slash + 1, slash + size + 1));
+            index = slash + size + 1;
         }
+            
+        return res;
+    }
+
+    // Encodes a list of strings to a single string.
+    public String encode_3r(List<String> strs) {
+        if (strs.size() == 0)
+            return null;
+        StringBuilder sb = new StringBuilder();
+        for(String str : strs) {
+            sb.append(str.length() + "/" + str);
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode_3r(String s) {
+        List<String> res = new ArrayList<>();
+        if (s == null)
+            return res;
+        
+        int index = 0;
+        while(index < s.length()) {
+            int slash = s.indexOf("/", index);
+            int size = Integer.valueOf(s.substring(index, slash));
+            String str = s.substring(slash + 1, slash + size + 1);
+            res.add(str);
+            index = slash + size + 1;
+        }
+        
+        return res;
+    }
 }
