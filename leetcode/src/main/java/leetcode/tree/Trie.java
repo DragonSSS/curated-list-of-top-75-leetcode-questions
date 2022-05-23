b91f6034f8ff8a53b4f716bb7258cd7e499e1706
DragonSSS <songweilong1989@gmail.com>
2022-05-23 07:54:10 -0400

    - update

--- leetcode/src/main/java/leetcode/tree/Trie.java
+++ leetcode/src/main/java/leetcode/tree/Trie.java
@@ -118,4 +118,56 @@ public class Trie {
         
         return true;
     }
+
+    // class TreeNode {
+    //     TreeNode[] children;
+    //     boolean isWord;
+        
+    //     public TreeNode() {
+    //         children = new TreeNode[26];
+    //         isWord = false;
+    //     }
+    // }
+    
+    // TreeNode root;
+    // /** Initialize your data structure here. */
+    // public Trie() {
+    //     root = new TreeNode();
+    // }
+    
+    /** Inserts a word into the trie. */
+    public void insert_3r(String word) {
+        TreeNode node = root;
+        for(char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TreeNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search_3r(String word) {
+        TreeNode node = root;
+        for(char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return node.isWord;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith_3r(String prefix) {
+        TreeNode node = root;
+        for(char c : prefix.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return true;
+    }
 }
