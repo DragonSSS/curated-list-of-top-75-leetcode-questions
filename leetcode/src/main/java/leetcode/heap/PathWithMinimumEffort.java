0c0bf15d403bd080b558500f65ae0e5ae39d7015
DragonSSS <songweilong1989@gmail.com>
2022-06-29 07:29:31 -0400

    - update

---
+++ leetcode/src/main/java/leetcode/heap/PathWithMinimumEffort.java
@@ -0,0 +1,8 @@
+package leetcode.heap;
+
+public class PathWithMinimumEffort {
+    // variant of Dijkstra algorithm
+    public int minimumEffortPath(int[][] heights) {
+        return 0;
+    }    
+}
