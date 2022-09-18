ede0a9970b324fd3624dc5353d13a78d0fb006ab
DragonSSS <songweilong1989@gmail.com>
2022-09-17 23:58:43 -0400

    - update

--- leetcode/src/main/java/leetcode/heap/SwimInRisingWater.java
+++ leetcode/src/main/java/leetcode/heap/SwimInRisingWater.java
@@ -35,5 +35,33 @@ public class SwimInRisingWater {
         }
         
         return level;
-    }    
+    }
+    // Dijkstra's algorithm by minHeap
+    // int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    public int swimInWater_2r(int[][] grid) {
+        int n = grid.length;
+        boolean[][] visited = new boolean[n][n];
+        int res = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> grid[a[0]][a[1]] - grid[b[0]][b[1]]);
+        pq.offer(new int[]{0, 0});
+        
+        while(!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            res = Math.max(res, grid[cur[0]][cur[1]]);
+            
+            if(cur[0] == n - 1 && cur[1] == n - 1)
+                return res;
+            
+            for(int[] dir : dirs) {
+                int x = cur[0] + dir[0];
+                int y = cur[1] + dir[1];
+                
+                if (x < 0 || x >= n || y < 0 || y >= n || visited[x][y])
+                    continue;
+                visited[x][y] = true;
+                pq.offer(new int[]{x, y});
+            }
+        }
+        return res;
+    }
 }
