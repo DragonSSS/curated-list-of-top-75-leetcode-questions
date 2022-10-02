76ed97712e9910c2b102d349378c55ed815e4e5b
DragonSSS <songweilong1989@gmail.com>
2022-10-02 00:48:57 -0400

    - update

--- leetcode/src/main/java/leetcode/heap/NetworkDelayTime.java
+++ leetcode/src/main/java/leetcode/heap/NetworkDelayTime.java
@@ -49,5 +49,48 @@ public class NetworkDelayTime {
         }
         
         return  visited.size() == n? res : - 1;
-    }    
+    }
+
+    // dijsktra algorithm
+    // build the graph by Map<Integer, List<int[]>>
+    // minHeap<int[]> pq store int[]{cost, node}
+    // Set<Integer> visited
+    public int networkDelayTime_2r(int[][] times, int n, int k) {
+        Set<Integer> visited = new HashSet<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        
+        // build graph
+        for(int[] time : times) {
+            int source = time[0];
+            int target = time[1];
+            int cost = time [2];
+            
+            graph.putIfAbsent(source, new ArrayList<>());
+            graph.get(source).add(new int[]{target, cost});
+        }
+        
+        pq.offer(new int[]{0, k});
+        int res = 0;
+        while(!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int curCost = cur[0];
+            int curNode = cur[1];
+            if (visited.contains(curNode))
+                continue;
+            visited.add(curNode);
+            res = curCost;
+            
+            if (graph.containsKey(curNode)) {
+                for(int[] next : graph.get(curNode)) {
+                    int nextNode = next[0];
+                    int addCost = next[1];
+                    if (visited.contains(nextNode))
+                        continue;
+                    pq.offer(new int[]{curCost + addCost, nextNode});
+                }
+            }
+        }
+        return visited.size() == n ? res : -1;
+    }
 }
