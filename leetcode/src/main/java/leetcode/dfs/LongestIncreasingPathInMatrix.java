b0e0f8ccbde148768de5458310bb4f2b69de9782
DragonSSS <songweilong1989@gmail.com>
2022-10-12 23:58:21 -0400

    - update

--- leetcode/src/main/java/leetcode/dfs/LongestIncreasingPathInMatrix.java
+++ leetcode/src/main/java/leetcode/dfs/LongestIncreasingPathInMatrix.java
@@ -40,4 +40,35 @@ public class LongestIncreasingPathInMatrix {
         memo[x][y] = res;
         return res;
     }
+
+    int[][] memo;
+    // int[][] dirs = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    public int longestIncreasingPath_2r(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        memo = new int[m][n];
+        int res = 1;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                res = Math.max(res, helper_2r(matrix, i, j, -1));
+            }
+        }
+        return res;
+    }
+    
+    private int helper_2r(int[][] matrix, int i, int j, int preVal) {
+        if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || matrix[i][j] <= preVal)
+            return 0;
+        
+        if (memo[i][j] != 0)
+            return memo[i][j];
+        
+        int curRes = 0;
+        for (int[] dir : dirs) {
+            curRes = Math.max(curRes, helper_2r(matrix, i + dir[0], j + dir[1], matrix[i][j]));
+        }
+        
+        memo[i][j] = curRes + 1;
+        return memo[i][j];
+    }
 }
