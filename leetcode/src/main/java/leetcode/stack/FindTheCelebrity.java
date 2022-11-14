ed7dae72cee853acb43c4055c069062b1ff23889
DragonSSS <songweilong1989@gmail.com>
2022-11-13 23:06:11 -0500

    - update

--- leetcode/src/main/java/leetcode/stack/FindTheCelebrity.java
+++ leetcode/src/main/java/leetcode/stack/FindTheCelebrity.java
@@ -40,4 +40,38 @@ public class FindTheCelebrity {
         }
         return ans;
     }
+
+    // a know b => knows(a, b);
+    // knows(x, celebrity) -> always true;
+    // knows(celebrity, x) -> always false;
+    // use stack to approach celerity as much as possible
+    // stack to store all possible ppl first, then try to poll up two
+    // use knows() api to check possible ppl
+    public int findCelebrity_2r(int n) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < n; i++) {
+            stack.push(i);
+        }
+        
+        while(stack.size() > 1) {
+            int ppl1 = stack.pop();
+            int ppl2 = stack.pop();
+            if (knows(ppl1, ppl2)) {
+                stack.push(ppl2);
+            } else { // dont use if (knows(ppl2, ppl1)) that could cause pop() on empty stack below
+                stack.push(ppl1);
+            }
+        }
+        
+        int res = stack.pop();
+        
+        for(int i = 0; i < n; i++) {
+            if (i == res)
+                continue;
+            
+            if (!knows(i, res) || knows(res, i))
+                return -1;
+        }
+        return res;
+    }    
 }
