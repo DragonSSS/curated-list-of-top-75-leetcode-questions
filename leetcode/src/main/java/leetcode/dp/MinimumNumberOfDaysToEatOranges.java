7b63b523352fb9af60cafa9f48a545f31556991f
DragonSSS <songweilong1989@gmail.com>
2022-12-06 19:34:12 -0500

    - update

--- leetcode/src/main/java/leetcode/dp/MinimumNumberOfDaysToEatOranges.java
+++ leetcode/src/main/java/leetcode/dp/MinimumNumberOfDaysToEatOranges.java
@@ -1,7 +1,11 @@
 package leetcode.dp;
 
 import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public class MinimumNumberOfDaysToEatOranges {
     Map<Integer, Integer> memo = new HashMap<>();
@@ -15,5 +19,41 @@ public class MinimumNumberOfDaysToEatOranges {
         int res = Math.min(n % 2 + minDays(n / 2), n % 3 + minDays(n / 3)) + 1;
         memo.put(n, res);
         return res;
-    }    
+    }
+    // bfs
+    public int minDays_bfs(int n) {
+        Set<Integer> visited = new HashSet<>();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(n);
+        // visited.add(n);
+        int res = 0;
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                int cur = queue.poll();
+                if (cur == 0)
+                    return res;
+
+                if (cur % 3 == 0) {
+                    if (!visited.contains(cur / 3)) {
+                        queue.offer(cur / 3);
+                        visited.add(cur / 3);
+                    }
+                }
+                if (cur % 2 == 0) {
+                    if (!visited.contains(cur / 2)) {
+                        queue.offer(cur / 2);
+                        visited.add(cur / 2);
+                    }
+                }
+                if (!visited.contains(cur - 1)) {
+                    queue.offer(cur - 1);
+                    visited.add(cur - 1);
+                }
+            }
+            res++;
+        }
+        return -1;
+    }
 }
