368cfec098f1f56ff7c491ceae57c892e43ac6eb
DragonSSS <songweilong1989@gmail.com>
2022-12-20 23:18:11 -0500

    - update

--- leetcode/src/main/java/leetcode/bfs/DetonateMaximumBombs.java
+++ leetcode/src/main/java/leetcode/bfs/DetonateMaximumBombs.java
@@ -1,5 +1,8 @@
 package leetcode.bfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class DetonateMaximumBombs {
     public int maximumDetonation(int[][] bombs) {
         int max = 0;
@@ -28,5 +31,29 @@ public class DetonateMaximumBombs {
         long distance = dx * dx + dy * dy;
         long r = b1[2];
         return distance <= r * r;
-    }    
+    }
+
+    // bfs version
+    private int helper_bfs(int index, int[][] bombs, boolean[] visited) {
+        int curCount = 1;
+        visited[index] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(index);
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                int cur = queue.poll();
+                for(int j = 0; j < bombs.length; j++) {
+                    if (visited[j])
+                        continue;
+                    if (isInRange(bombs[cur], bombs[j])) {
+                        queue.offer(j);
+                        curCount++;
+                        visited[j] = true;
+                    }
+                }
+            }
+        }
+        return curCount;
+    }
 }
