d834ae2f6462d2a9f34d69e524c84c5805382f75
DragonSSS <songweilong1989@gmail.com>
2022-12-30 23:15:39 -0500

    - update

---
+++ leetcode/src/main/java/leetcode/string/NumberOfMatchingSubsequences.java
@@ -0,0 +1,8 @@
+package leetcode.string;
+
+public class NumberOfMatchingSubsequences {
+    // trie + dfs
+    public int numMatchingSubseq(String s, String[] words) {
+        return 0;
+    }    
+}
