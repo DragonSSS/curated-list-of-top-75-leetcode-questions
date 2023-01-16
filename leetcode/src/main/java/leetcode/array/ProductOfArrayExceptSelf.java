57b666b516757796773acc74a6864bea5ffba4d2
DragonSSS <songweilong1989@gmail.com>
2023-01-15 22:41:46 -0500

    -update

--- leetcode/src/main/java/leetcode/array/ProductOfArrayExceptSelf.java
+++ leetcode/src/main/java/leetcode/array/ProductOfArrayExceptSelf.java
@@ -43,4 +43,25 @@ public class ProductOfArrayExceptSelf {
         
         return res;
     }
+
+    // 1 2 3 4
+    // left to right, prefix product = 1, 1, 2, 6
+    // right to left, prefix product = 24 12 4  1
+    public int[] productExceptSelf_3r(int[] nums) {
+        int size = nums.length;
+        int[] leftPrefix = new int[size];
+        leftPrefix[0] = 1;
+        for (int i = 1; i < size; i++) {
+            leftPrefix[i] = leftPrefix[i - 1] * nums[i - 1];
+        }
+        
+        int[] result = new int[size];
+        int right = 1;
+        for (int i = size - 1; i >= 0; i--) {
+            result[i] = right * leftPrefix[i];
+            right *= nums[i];
+        }
+        
+        return result;
+    }
 }
