7722e67fcf49bb7edd1c7da7850c5d53ddd9eccb
DragonSSS <songweilong1989@gmail.com>
2023-01-31 21:41:35 -0500

    - update

--- leetcode/src/main/java/leetcode/dp/DeleteAndEarn.java
+++ leetcode/src/main/java/leetcode/dp/DeleteAndEarn.java
@@ -26,5 +26,19 @@ public class DeleteAndEarn {
         int res = Math.max(helper(maps, index + 1, memo), helper(maps, index + 2, memo) + index * maps[index]);
         memo[index] = res;
         return res;
-    }    
+    }
+    
+    public int deleteAndEarn_dp(int[] nums) {
+        int[] sum = new int[100001];
+        int max = 0;
+        for(int num : nums) {
+            sum[num] += num;
+            max = Math.max(max, num);
+        }
+        
+        for(int i = 2; i <= max; i++) {
+            sum[i] = Math.max(sum[i - 1], sum[i - 2] + sum[i]);
+        }
+        return sum[max];
+    }
 }
