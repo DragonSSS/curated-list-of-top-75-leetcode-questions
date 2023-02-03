48afc34f22649fbaa486f6618b746ea97968b3a0
DragonSSS <songweilong1989@gmail.com>
2023-02-03 00:31:50 -0500

    - update

--- leetcode/src/main/java/leetcode/tree/SumRootToLeafNumbers.java
+++ leetcode/src/main/java/leetcode/tree/SumRootToLeafNumbers.java
@@ -17,4 +17,19 @@ public class SumRootToLeafNumbers {
 
         return helper(node.left, currentSum) + helper(node.right, currentSum);
     }
+    
+    private int helper_2r(TreeNode node, int sum) {
+        // if we have only one side of tree
+        // if (node == null)
+        //     return 0;
+        
+        int curSum = sum * 10 + node.val;
+        
+        // when reach the leaf, should return current sum
+        if (node.left == null && node.right == null)
+            return curSum;
+        
+        return (node.left == null ? 0 : helper(node.left, curSum)) +
+             (node.right == null ? 0 : helper(node.right, curSum));
+    }
 }
