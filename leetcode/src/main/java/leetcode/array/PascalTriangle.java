7972a3190bf0feba21cdab71277020b4ed1f6bfe
DragonSSS <songweilong1989@gmail.com>
2023-02-12 23:33:30 -0500

    - update

---
+++ leetcode/src/main/java/leetcode/array/PascalTriangle.java
@@ -0,0 +1,23 @@
+package leetcode.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < numRows; i++) {
+            List<Integer> cur = new ArrayList<>();
+            for(int j = 0; j <= i; j++) {
+                if(j == 0 || j == i) {
+                    cur.add(1);
+                } else {
+                    cur.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
+                }
+                
+            }
+            res.add(cur);
+        }
+        return res;
+    }    
+}
