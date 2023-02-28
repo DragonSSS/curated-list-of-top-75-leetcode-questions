4ad6a471f15562a012948490822cdb982e018881
DragonSSS <songweilong1989@gmail.com>
2023-02-27 22:43:03 -0500

    - update

--- leetcode/src/main/java/leetcode/dp/MinimumNumberOfDaysToEatOranges.java
+++ leetcode/src/main/java/leetcode/dp/MinimumNumberOfDaysToEatOranges.java
@@ -56,4 +56,33 @@ public class MinimumNumberOfDaysToEatOranges {
         }
         return -1;
     }
+
+    // Map<Integer, Integer> memo = new HashMap<>();
+    public int minDays_dfs(int n) {
+        if (n == 1)
+            return 1;
+        
+        if (n <= 3)
+            return 2;
+        
+        if (memo.containsKey(n))
+            return memo.get(n);
+        
+        int cur = n;
+        if(n % 2 == 0) {
+            cur = Math.min(cur, minDays(n / 2) + 1);
+        } else {
+            cur = Math.min(cur, minDays(n - 1) + 1);
+        }
+        
+        if(n % 3 == 0) {
+            cur = Math.min(cur, minDays(n / 3) + 1);
+        } else if (n % 3 == 1) {
+            cur = Math.min(cur, minDays(n - 1) + 1);
+        } else {
+            cur = Math.min(cur, minDays(n - 2) + 2);
+        }
+        memo.put(n, cur);
+        return cur;
+    }
 }
