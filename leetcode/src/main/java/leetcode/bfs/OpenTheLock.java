ac226bacbee93fc57fe5e13ccbc520f7a8dbb714
DragonSSS <songweilong1989@gmail.com>
2023-02-28 22:00:25 -0500

    - update

--- leetcode/src/main/java/leetcode/bfs/OpenTheLock.java
+++ leetcode/src/main/java/leetcode/bfs/OpenTheLock.java
@@ -58,7 +58,53 @@ public class OpenTheLock {
         return res;
     }
     
+    // bfs
     public int openLock_2r(String[] deadends, String target) {
-        return 0;
+        Set<String> visited = new HashSet<>(Arrays.asList(deadends));
+        if(visited.contains("0000"))
+            return -1;
+        Queue<String> queue = new LinkedList<>();
+        queue.offer("0000");
+        visited.add("0000");
+        
+        int res = 0;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            for(int i = 0; i < size; i++) {
+                String cur = queue.poll();
+                
+                if(target.equals(cur))
+                    return res;
+                
+                List<String> nexts = findNexts_2r(cur);
+                
+                for(String next : nexts) {
+                    if(visited.contains(next))
+                        continue;
+                    queue.offer(next);
+                    visited.add(next);
+                }
+            }
+            res++;
+        }
+        return -1;
+    }
+    
+    private List<String> findNexts_2r(String s) {
+        List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder(s);
+        
+        for(int i = 0; i < 4; i++) {
+            char c = sb.charAt(i);
+            int num = (int) (c - '0');
+            int plus = (num + 1) % 10;
+            int minus = (num - 1 + 10) % 10;
+            sb.setCharAt(i, (char)(plus + '0'));
+            res.add(sb.toString());
+            sb.setCharAt(i, (char)(minus + '0'));
+            res.add(sb.toString());
+            sb.setCharAt(i, c);
+        }
+        return res;
     }
 }
