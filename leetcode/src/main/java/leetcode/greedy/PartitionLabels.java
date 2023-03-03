82ae15bae7b6e50d78994bbcb0363df12a4692a2
DragonSSS <songweilong1989@gmail.com>
2023-03-02 22:56:14 -0500

    - update

--- leetcode/src/main/java/leetcode/greedy/PartitionLabels.java
+++ leetcode/src/main/java/leetcode/greedy/PartitionLabels.java
@@ -21,5 +21,26 @@ public class PartitionLabels {
             }
         }
         return res;
-    }    
+    }
+
+    // hashtable + two pointers
+    public List<Integer> partitionLabels_2r(String s) {
+        int[] lastIndex = new int[26];
+        List<Integer> res = new ArrayList<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            lastIndex[s.charAt(i) - 'a'] = i;
+        }
+        
+        int start = 0, end = 0;
+        for(int i = 0; i < s.length(); i++) {
+            end = Math.max(end, lastIndex[s.charAt(i) - 'a']);
+            if (i == end) {
+                res.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        
+        return res;
+    }
 }
