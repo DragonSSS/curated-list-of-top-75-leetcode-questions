f895013904857bdb78cc261aaa905d93d7607d74
DragonSSS <songweilong1989@gmail.com>
2023-03-15 21:58:40 -0400

    - update

--- leetcode/src/main/java/leetcode/array/ArrayWithElementsNotEqualToAverageOfNeighbors.java
+++ leetcode/src/main/java/leetcode/array/ArrayWithElementsNotEqualToAverageOfNeighbors.java
@@ -3,25 +3,24 @@ package leetcode.array;
 import java.util.Arrays;
 
 public class ArrayWithElementsNotEqualToAverageOfNeighbors {
-    public int numSubseq(int[] nums, int target) {
-        int n = nums.length, left = 0, right = nums.length - 1, res = 0;
-        int[] pows = new int[n];
-        int mod = (int)1e9 + 7;
-        
-        pows[0] = 1;
-        for(int i = 1; i < n; i++) {
-            pows[i] = (pows[i - 1] * 2) % mod;
-        }
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length, left = 0, right = nums.length - 1;
+        int[] res = new int[n];
         
         Arrays.sort(nums);
-        while(left <= right) {
-           if (nums[left] + nums[right] > target) {
-               right--;
-           } else {
-               res = (res + pows[right - left]) % mod;
-               left++;
-           }
+        int index = 0;
+        
+        // small, big, small, big
+        while(left < right) {
+            res[index++] = nums[left++];
+            res[index++] = nums[right--];
         }
+        
+        // odd case
+        if (left == right) {
+            res[index++] = nums[left];
+        }
+        
         return res;
-    }    
+    }   
 }
