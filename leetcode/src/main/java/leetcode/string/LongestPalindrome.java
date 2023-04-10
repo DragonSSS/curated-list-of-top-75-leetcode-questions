5079ddb8b5aa3b8d699b355d28ca83cba6f79f4e
DragonSSS <songweilong1989@gmail.com>
2023-04-09 21:21:54 -0400

    - update

--- leetcode/src/main/java/leetcode/string/LongestPalindrome.java
+++ leetcode/src/main/java/leetcode/string/LongestPalindrome.java
@@ -23,4 +23,27 @@ public class LongestPalindrome {
 
         return res;
     }
+
+    public int longestPalindrome_2r(String s) {
+        int[] map = new int[128];
+        for(char c : s.toCharArray()) {
+            map[c]++;
+        }
+
+        int res = 0;
+        boolean flag = false;
+        for(int i = 0; i < 128; i++) {
+            if(map[i] % 2 == 0) {
+                res+= map[i];
+            } else {
+                if (!flag) {
+                    res += map[i];
+                    flag = true;
+                } else {
+                    res += map[i] - 1;
+                }
+            }
+        }
+        return res;
+    }
 }
