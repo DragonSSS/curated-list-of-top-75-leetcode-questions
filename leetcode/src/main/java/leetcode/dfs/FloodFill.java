e01548f1fafac19fe8ec78fd399a68d4304307f2
DragonSSS <songweilong1989@gmail.com>
2023-04-11 19:53:10 -0400

    - update

--- leetcode/src/main/java/leetcode/dfs/FloodFill.java
+++ leetcode/src/main/java/leetcode/dfs/FloodFill.java
@@ -16,4 +16,21 @@ public class FloodFill {
             helper(image, x + dir[0], y + dir[1], newColor, curColor);
         }
     }
+
+    //int[][] dirs = new int[][]{{0,1}, {1,0}, {0,-1}, {-1,0}};
+    public int[][] floodFill_2r(int[][] image, int sr, int sc, int color) {
+        helper_2r(image, sr, sc, image[sr][sc], color);
+        return image;
+    }
+
+    private void helper_2r(int[][] image, int x, int y, int curColor, int newColor) {
+        if (x < 0 || x >= image.length || y < 0 || y >= image[0].length || image[x][y] != curColor || image[x][y] == newColor) {
+            return;
+        }
+
+        image[x][y] = newColor;
+        for(int[] dir : dirs) {
+            helper_2r(image, x + dir[0], y + dir[1], curColor, newColor);
+        }
+    }
 }
