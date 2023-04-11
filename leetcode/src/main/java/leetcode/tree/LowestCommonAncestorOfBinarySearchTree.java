142f1ccda16b8c832159dbf188b9da9a125f20f5
DragonSSS <songweilong1989@gmail.com>
2023-04-10 22:44:26 -0400

    - update

--- leetcode/src/main/java/leetcode/tree/LowestCommonAncestorOfBinarySearchTree.java
+++ leetcode/src/main/java/leetcode/tree/LowestCommonAncestorOfBinarySearchTree.java
@@ -33,4 +33,14 @@ public class LowestCommonAncestorOfBinarySearchTree {
             return root;
         }
     }
+
+    public TreeNode lowestCommonAncestor_4r(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
 }
