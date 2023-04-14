449996aefd6cd107a3eb8f26a99403c3502da486
DragonSSS <songweilong1989@gmail.com>
2023-04-13 23:20:27 -0400

    - update

--- leetcode/src/main/java/leetcode/dp/UniquePaths.java
+++ leetcode/src/main/java/leetcode/dp/UniquePaths.java
@@ -93,4 +93,19 @@ public class UniquePaths {
         
         return dp[m - 1][n - 1];
     }
+
+    public int uniquePaths_4r_dp(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int[] arr : dp) {
+            Arrays.fill(arr, 1);
+        }
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
 }
