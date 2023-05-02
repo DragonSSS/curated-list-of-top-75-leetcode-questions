496ad1b515ec80c3dc531adeab7b2ccb07487258
DragonSSS <songweilong1989@gmail.com>
2023-05-01 23:53:02 -0400

    - update

--- leetcode/src/main/java/leetcode/binarysearch/Search2dMatrix.java
+++ leetcode/src/main/java/leetcode/binarysearch/Search2dMatrix.java
@@ -22,5 +22,22 @@ public class Search2dMatrix {
             }
         }
         return false;
-    }    
+    }
+
+    public boolean searchMatrix_2r(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int left = 0, right = m * n - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            int val = matrix[mid / n][mid % n];
+            if (val == target) {
+                return true;
+            } else if (val > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
 }
