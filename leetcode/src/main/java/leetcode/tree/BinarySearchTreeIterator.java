56dcd66ef038e8e4222c86ce7cdbfa816e0bab92
DragonSSS <songweilong1989@gmail.com>
2023-05-02 22:25:11 -0400

    - update

--- leetcode/src/main/java/leetcode/tree/BinarySearchTreeIterator.java
+++ leetcode/src/main/java/leetcode/tree/BinarySearchTreeIterator.java
@@ -1,8 +1,7 @@
 package leetcode.tree;
 
-import util.TreeNode;
-
 import java.util.Stack;
+import util.TreeNode;
 
 public class BinarySearchTreeIterator {
     Stack<TreeNode> stack;
@@ -24,6 +23,29 @@ public class BinarySearchTreeIterator {
 
     private void add(TreeNode node) {
         while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+    }
+
+    // Stack<TreeNode> stack;
+    // public BinarySearchTreeIterator_2r(TreeNode root) {
+    //     stack = new Stack<>();
+    //     add_2r(root);
+    // }
+    
+    public int next_2r() {
+        TreeNode node = stack.pop();
+        add_2r(node.right);
+        return node.val;
+    }
+    
+    public boolean hasNext_2r() {
+        return !stack.isEmpty();
+    }
+
+    private void add_2r(TreeNode node) {
+        while(node != null) {
             stack.push(node);
             node = node.left;
         }
