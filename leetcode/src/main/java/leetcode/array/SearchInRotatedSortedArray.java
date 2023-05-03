0340d1ce9bcc681d191bdd42824bb137c57f8c15
DragonSSS <songweilong1989@gmail.com>
2023-05-02 21:58:55 -0400

    - update

--- leetcode/src/main/java/leetcode/array/SearchInRotatedSortedArray.java
+++ leetcode/src/main/java/leetcode/array/SearchInRotatedSortedArray.java
@@ -123,4 +123,29 @@ public class SearchInRotatedSortedArray {
         
         return -1;
     }
+
+    public int search_5r(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] > target || target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if (nums[right] < target || target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
