f98f6224fbc4329eed7d7b03c68957f342c8e8f9
DragonSSS <songweilong1989@gmail.com>
2023-05-07 23:46:55 -0400

    - update

--- leetcode/src/main/java/leetcode/tree/Trie.java
+++ leetcode/src/main/java/leetcode/tree/Trie.java
@@ -170,4 +170,53 @@ public class Trie {
         }
         return true;
     }
+
+    class TrieNode {
+        public TrieNode[] children;
+        public boolean isWord;
+
+        public TrieNode() {
+            children = new TrieNode[26];
+            isWord = false;
+        }
+    }
+
+    TrieNode rootNode;
+    // public Trie() {
+    //     rootNode = new TrieNode();
+    // }
+    
+    public void insert_4r(String word) {
+        TrieNode node = rootNode;
+        for(char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isWord = true;
+    }
+    
+    public boolean search_4r(String word) {
+        TrieNode node = rootNode;
+        for(char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+
+        return node.isWord;
+    }
+    
+    public boolean startsWith_4r(String prefix) {
+        TrieNode node = rootNode;
+        for(char c : prefix.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return true;
+    }
 }
