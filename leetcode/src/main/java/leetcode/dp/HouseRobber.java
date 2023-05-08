abe3610412790df89f97040f5cea3b9aa3b59f86
DragonSSS <songweilong1989@gmail.com>
2023-05-07 23:29:13 -0400

    - update

--- leetcode/src/main/java/leetcode/dp/HouseRobber.java
+++ leetcode/src/main/java/leetcode/dp/HouseRobber.java
@@ -41,27 +41,50 @@ public class HouseRobber {
         return res;
     }
 
-        // O(n)
-        Integer[] memo_3r;
-        public int rob_3r(int[] nums) {
-            memo_3r = new Integer[nums.length];
-            return helper_3r(0, nums);
-        }
+    // O(n)
+    Integer[] memo_3r;
+    public int rob_3r(int[] nums) {
+        memo_3r = new Integer[nums.length];
+        return helper_3r(0, nums);
+    }
         
-        private int helper_3r(int index, int[] nums) {
-            if (index >= nums.length) {
-                return 0;
-            }
-            
-            if (memo_3r[index] != null) {
-                return memo_3r[index];
-            }
-            
-            int with = helper_3r(index + 2, nums) + nums[index];
-            int without = helper_3r(index + 1, nums);
-            
-            int res = Math.max(with, without);
-            memo_3r[index] = res;
-            return res;
+    private int helper_3r(int index, int[] nums) {
+        if (index >= nums.length) {
+            return 0;
         }
+            
+        if (memo_3r[index] != null) {
+            return memo_3r[index];
+        }
+            
+        int with = helper_3r(index + 2, nums) + nums[index];
+        int without = helper_3r(index + 1, nums);
+            
+        int res = Math.max(with, without);
+        memo_3r[index] = res;
+        return res;
+    }
+
+    // Integer[] memo;
+    public int rob_4r(int[] nums) {
+        memo = new Integer[nums.length];
+        return helper_4r(nums, 0);
+    }
+
+    private int helper_4r(int[] nums, int index) {
+        if (index >= nums.length) {
+            return 0;
+        }
+
+        if (memo[index] != null) {
+            return memo[index];
+        }
+
+        int with = helper_4r(nums, index + 2) + nums[index];
+        int without = helper_4r(nums, index + 1);
+
+        int res = Math.max(with, without);
+        memo[index] = res;
+        return res;
+    }
 }
