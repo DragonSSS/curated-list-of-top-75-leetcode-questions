86a3487ee136ba2ad510b28ae8c45e98a91a903f
DragonSSS <songweilong1989@gmail.com>
2023-05-09 21:41:33 -0400

    - update

--- leetcode/src/main/java/leetcode/design/MinStack.java
+++ leetcode/src/main/java/leetcode/design/MinStack.java
@@ -32,4 +32,26 @@ public class MinStack {
     public int getMin() {
         return minStack.peek();
     }
+
+    public void push_2r(int val) {
+        if(minStack.isEmpty() || minStack.peek() >= val) {
+            minStack.push(val);
+        }
+        stack.push(val);
+    }
+    
+    public void pop_2r() {
+        if(stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+    
+    public int top_2r() {
+        return stack.peek();
+    }
+    
+    public int getMin_2r() {
+        return minStack.peek();
+    }
 }
