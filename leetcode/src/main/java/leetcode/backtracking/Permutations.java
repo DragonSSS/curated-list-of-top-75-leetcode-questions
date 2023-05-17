1a5507614d21931cb3bba00e9e7cbb5b347151fc
DragonSSS <songweilong1989@gmail.com>
2023-05-16 22:01:14 -0400

    - update

--- leetcode/src/main/java/leetcode/backtracking/Permutations.java
+++ leetcode/src/main/java/leetcode/backtracking/Permutations.java
@@ -23,5 +23,28 @@ public class Permutations {
             helper(nums, curArray);
             curArray.remove(curArray.size() - 1);
         }
-    }  
+    }
+
+    // List<List<Integer>> res;
+    public List<List<Integer>> permute_2r(int[] nums) {
+        res = new ArrayList<>();
+        helper_2r(nums, new ArrayList<>());
+        return res;
+    }
+
+    private void helper_2r(int[] nums, List<Integer> curList) {
+        if(curList.size() == nums.length) {
+            res.add(new ArrayList<>(curList));
+            return;
+        }
+
+        for(int num : nums) {
+           if(curList.contains(num)) {
+               continue;
+           }
+           curList.add(num);
+           helper_2r(nums, curList);
+           curList.remove(curList.size() - 1);
+        }
+    }
 }
