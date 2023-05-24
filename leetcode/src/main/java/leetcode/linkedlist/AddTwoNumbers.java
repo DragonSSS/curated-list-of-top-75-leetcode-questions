6fda60d4d371c4b3065c30bf9f971995ae7f0bd2
DragonSSS <songweilong1989@gmail.com>
2023-05-23 22:05:39 -0400

    - update

--- leetcode/src/main/java/leetcode/linkedlist/AddTwoNumbers.java
+++ leetcode/src/main/java/leetcode/linkedlist/AddTwoNumbers.java
@@ -25,5 +25,31 @@ public class AddTwoNumbers {
                 l2 = l2.next;
         }
         return dummy.next;
-    }    
+    }
+
+    public ListNode addTwoNumbers_2r(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode node = dummy;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0) {
+            int val1 = l1 == null? 0 : l1.val;
+            int val2 = l2 == null? 0 : l2.val;
+
+            int sum = val1 + val2 + carry;
+            int rest = sum % 10;
+            carry = sum / 10;
+            ListNode cur = new ListNode(rest);
+            node.next = cur;
+            node = node.next;
+
+            if(l1 != null) {
+                l1 = l1.next;
+            }
+
+            if(l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return dummy.next;
+    }
 }
