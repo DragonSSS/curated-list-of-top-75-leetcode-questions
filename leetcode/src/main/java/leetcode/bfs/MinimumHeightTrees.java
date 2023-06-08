46cf8f2fbeb6309c5d673f0e06d26a4e001728bb
DragonSSS <songweilong1989@gmail.com>
2023-06-08 00:00:59 -0400

    - update

--- leetcode/src/main/java/leetcode/bfs/MinimumHeightTrees.java
+++ leetcode/src/main/java/leetcode/bfs/MinimumHeightTrees.java
@@ -1,9 +1,12 @@
 package leetcode.bfs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class MinimumHeightTrees {
@@ -36,6 +39,53 @@ public class MinimumHeightTrees {
                 graph.get(j).remove(i);
                 if (graph.get(j).size() == 1)
                     newLeaves.add(j);
+            }
+            leaves = newLeaves;
+        }
+        return leaves;
+    }
+
+    // Thinking about if we have single path here
+    // to find the mini height tree, we will have two pointers from each end, move them with same speed until
+    // they meet. middle node(s) is the root we want
+
+    // It is similar to BFS topolical sort, remove the leaves, iterate level by level,
+    // remove new leaves if out-degree is 1
+    // until 1 or 2 node(s) left
+    
+    public List<Integer> findMinHeightTrees_2r(int n, int[][] edges) {
+        if (n == 1) {
+            return Arrays.asList(0);
+        }
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+
+        for(int i = 0; i < n; i++) {
+            graph.put(i, new HashSet<>());
+        }
+
+        for(int[] edge : edges) {
+            int from = edge[0];
+            int to = edge [1];
+            graph.get(from).add(to);
+            graph.get(to).add(from);
+        }
+
+        List<Integer> leaves = new ArrayList<>();
+        for(int key : graph.keySet()){
+            if(graph.get(key).size() == 1) {
+                leaves.add(key);
+            }
+        }
+
+        while( n > 2) {
+            n -= leaves.size();
+            List<Integer> newLeaves = new ArrayList<>();
+            for(int i : leaves) {
+                int j = graph.get(i).iterator().next();
+                graph.get(j).remove(i);
+                if (graph.get(j).size() == 1) {
+                    newLeaves.add(j);
+                }
             }
             leaves = newLeaves;
         }
