a9cd6e14c89caff0dc3cf26c51d6ca7d471255f2
DragonSSS <songweilong1989@gmail.com>
2023-06-19 21:26:50 -0400

    - update

--- leetcode/src/main/java/leetcode/queue/SlidingWindowMaximum.java
+++ leetcode/src/main/java/leetcode/queue/SlidingWindowMaximum.java
@@ -1,9 +1,8 @@
 package leetcode.queue;
 
-import java.util.PriorityQueue;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class SlidingWindowMaximum {
     // [1,3,-1,-3,5,3,6,7], k = 3
@@ -42,6 +41,29 @@ public class SlidingWindowMaximum {
             deque.offerLast(i);
 
             if (i >= k - 1) {
+                res[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+        return res;
+    }
+
+    public int[] maxSlidingWindow_2r(int[] nums, int k) {
+        int size = nums.length;
+        int[] res = new int[size - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>();
+        for(int i = 0; i < size; i++) {
+            // remove leftmost index that is out of sliding window size k
+            while(!deque.isEmpty() && deque.peekFirst() < i - k + 1){
+                deque.pollFirst();
+            }
+
+            // remove rightmost index that value of array is smaller than current value
+            while(!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+
+            if( i - k + 1 >= 0) {
                 res[i - k + 1] = nums[deque.peekFirst()];
             }
         }
