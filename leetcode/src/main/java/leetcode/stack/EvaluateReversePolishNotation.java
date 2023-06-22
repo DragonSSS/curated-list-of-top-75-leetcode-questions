0dfec7bf60bf44cd82bbbf0426941e491d246f8d
DragonSSS <songweilong1989@gmail.com>
2023-06-22 18:04:18 -0400

    - update

--- leetcode/src/main/java/leetcode/stack/EvaluateReversePolishNotation.java
+++ leetcode/src/main/java/leetcode/stack/EvaluateReversePolishNotation.java
@@ -6,8 +6,7 @@ import java.util.Set;
 import java.util.Stack;
 
 public class EvaluateReversePolishNotation {
-    // 2, 1, + 3, *
-    // two stacks
+    // one stack
     public int evalRPN(String[] tokens) {
         Stack<Integer> operands = new Stack<>();
         Set<String> operatorSet = new HashSet<>(Arrays.asList("+", "-", "*", "/"));
@@ -44,5 +43,31 @@ public class EvaluateReversePolishNotation {
                 break;
         }
         return res;
-    }    
+    }
+
+    public int evalRPN_2r(String[] tokens) {
+        Stack<Integer> nums = new Stack<>();
+        Set<String> opsSet = new HashSet<>(Arrays.asList("+", "-", "*", "/"));
+        for(String token : tokens) {
+            if(opsSet.contains(token)) {
+                nums.push(compute_2r(token, nums.pop(), nums.pop()));
+            } else {
+                nums.push(Integer.valueOf(token));
+            }
+        }
+        return nums.peek();
+    }
+
+    private int compute_2r(String op, int num1, int num2) {
+        if (op.equals("+")) {
+            return num1 + num2;
+        } else if (op.equals("-")) {
+            return num2 - num1; // reverse
+        } else if (op.equals("*")) {
+            return num1 * num2;
+        } else if (op.equals("/")) {
+            return num2 / num1; // reverse
+        }
+        return 0;
+    }
 }
