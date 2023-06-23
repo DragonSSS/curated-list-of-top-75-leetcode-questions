02f1bd0e11761b93bf8f5b9e9401133cc845679f
DragonSSS <songweilong1989@gmail.com>
2023-06-22 22:11:38 -0400

    - update

--- leetcode/src/main/java/leetcode/stack/DailyTemperatures.java
+++ leetcode/src/main/java/leetcode/stack/DailyTemperatures.java
@@ -15,5 +15,19 @@ public class DailyTemperatures {
             stack.push(i);
         }
         return res;
-    }    
+    }
+
+    public int[] dailyTemperatures_2r(int[] temperatures) {
+        // monotonic stack
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[temperatures.length];
+        for(int i = 0; i < temperatures.length; i++) {
+            while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                int index = stack.pop();
+                res[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
 }
