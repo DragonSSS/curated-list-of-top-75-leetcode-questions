62fae35bfd5e1cfa301ad0f01175d06f1c3fa8b4
DragonSSS <songweilong1989@gmail.com>
2023-07-05 23:01:24 -0400

    - update

--- leetcode/src/main/java/leetcode/dp/LongestIncreasingSubsequence.java
+++ leetcode/src/main/java/leetcode/dp/LongestIncreasingSubsequence.java
@@ -68,4 +68,19 @@ public class LongestIncreasingSubsequence {
         
         return res;
     }
+
+    public int lengthOfLIS_4r(int[] nums) {
+        int[] dp = new int[nums.length];
+        int res = 1;
+        Arrays.fill(dp, 1);
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    res = Math.max(res, dp[i]);
+                }
+            }
+        }
+        return res;
+    }
 }
