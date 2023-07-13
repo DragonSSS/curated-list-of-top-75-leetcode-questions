6a1b77f595e327e2bccd09abeeef83c513b4b856
DragonSSS <songweilong1989@gmail.com>
2023-07-13 00:22:22 -0400

    - update

--- leetcode/src/main/java/leetcode/binarysearch/FindKClosestElements.java
+++ leetcode/src/main/java/leetcode/binarysearch/FindKClosestElements.java
@@ -1,5 +1,6 @@
 package leetcode.binarysearch;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -19,5 +20,22 @@ public class FindKClosestElements {
         }
         Arrays.sort(res);
         return Arrays.asList(res);
-    }    
+    }
+
+    public List<Integer> findClosestElements_binary_search(int[] arr, int k, int x) {
+        int left = 0, right = arr.length - 1;
+        while(right - left >= k) {
+            if(Math.abs(arr[left] - x) > Math.abs(arr[right] - x)) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for(int i = left; i <= right; i++) {
+            res.add(arr[i]);
+        }
+        return res;
+    }
 }
