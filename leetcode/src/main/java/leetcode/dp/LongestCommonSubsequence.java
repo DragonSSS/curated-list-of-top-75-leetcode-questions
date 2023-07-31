31c8ac26b1e6effae92659d7ca248d7fb5574ec7
Wilson Song <songweilong1989@gmail.com>
2023-07-31 08:53:00 -0400

    Update LongestCommonSubsequence.java

--- leetcode/src/main/java/leetcode/dp/LongestCommonSubsequence.java
+++ leetcode/src/main/java/leetcode/dp/LongestCommonSubsequence.java
@@ -92,8 +92,8 @@ public class LongestCommonSubsequence {
 
     public int longestCommonSubsequence_4r(String text1, String text2) {
         int[][] dp = new int[text1.length() + 1][text2.length() + 1];
-        for(int i = text1.length() - 1; i >=0; i--) {
-            for(int j = text2.length() - 1; j >=0; j--) {
+        for(int i = text1.length() - 1; i >= 0; i--) {
+            for(int j = text2.length() - 1; j >= 0; j--) {
                 if(text1.charAt(i) == text2.charAt(j)) {
                     dp[i][j] = dp[i + 1][j + 1] + 1;
                 } else {
