be5881995432d081a56e32e5353989b61febebc0
DragonSSS <songweilong1989@gmail.com>
2023-08-02 01:08:43 +0800

    - update

--- leetcode/src/main/java/leetcode/stack/NextGreaterElementII.java
+++ leetcode/src/main/java/leetcode/stack/NextGreaterElementII.java
@@ -1,7 +1,7 @@
 package leetcode.stack;
 
-import java.util.Stack;
 import java.util.Arrays;
+import java.util.Stack;
 
 public class NextGreaterElementII {
     public int[] nextGreaterElements(int[] nums) {
@@ -35,6 +35,23 @@ public class NextGreaterElementII {
             }
             if (i < size)
                 stack.push(i);
+        }
+        return res;
+    }
+
+    public int[] nextGreaterElements_3r(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < n * 2; i++) {
+            while(!stack.isEmpty() && nums[stack.peek()] < nums[i % n]) {
+                res[stack.pop()] = nums[i % n];
+            }
+
+            if (i < n) {
+                stack.push(i);
+            }
         }
         return res;
     }
