cde6a5de685a37ec4978219ae5824ceadb25d175
DragonSSS <songweilong1989@gmail.com>
2023-08-15 22:16:23 -0400

    - update

--- leetcode/src/main/java/leetcode/stack/NextGreaterElementI.java
+++ leetcode/src/main/java/leetcode/stack/NextGreaterElementI.java
@@ -51,6 +51,7 @@ public class NextGreaterElementI {
             }
             index++;
         }
+
         return res;
     }
 
@@ -69,6 +70,7 @@ public class NextGreaterElementI {
         for(int num : nums1) {
             res[index++] = map.getOrDefault(num, -1);
         }
+        
         return res;
     }
 }
