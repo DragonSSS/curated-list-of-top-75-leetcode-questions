cc74bea582999317ccc391ae68a4f661f6bfc60a
DragonSSS <songweilong1989@gmail.com>
2023-08-29 22:44:34 -0400

    - update

--- leetcode/src/main/java/leetcode/heap/ProcessTasksUsingServers.java
+++ leetcode/src/main/java/leetcode/heap/ProcessTasksUsingServers.java
@@ -39,5 +39,39 @@ public class ProcessTasksUsingServers {
         }
 
         return res;
-    }    
+    }
+
+    // two minHeap
+    // server {index, weight, availableTime}
+    public int[] assignTasks_2r(int[] servers, int[] tasks) {
+        // weight first, then index
+        PriorityQueue<int[]> freeSevrers = new PriorityQueue<>((a, b) -> a[1] != b[1]? a[1] - b[1] : a[0] - b[0]);
+        // available time first, then weight, finally index
+        PriorityQueue<int[]> usedServers = new PriorityQueue<>((a, b) -> a[2] != b[2]? a[2] - b[2] : (a[1] != b[1] ? a[1] - b[1]: a[0] - b[0]));
+
+        for(int i = 0; i < servers.length; i++) {
+            freeSevrers.offer(new int[] {i, servers[i], 0});
+        }
+
+        int[] res = new int[tasks.length];
+        for(int i = 0; i < tasks.length; i++) {
+            int processTime = tasks[i];
+            while(!usedServers.isEmpty() && usedServers.peek()[2] <= i) {
+                freeSevrers.offer(usedServers.poll());
+            }
+
+            int[] cur = new int[3];
+            if (!freeSevrers.isEmpty()) {
+                cur = freeSevrers.poll();
+                cur[2] = i + processTime; // diff
+                usedServers.offer(cur);
+            } else {
+                cur = usedServers.poll();
+                cur[2] += processTime; // diff
+                usedServers.offer(cur);
+            }
+            res[i] = cur[0];
+        }
+        return res;
+    }
 }
