c9f512b3db9d90780df5939c037ea02ff1b8e2f7
DragonSSS <songweilong1989@gmail.com>
2023-08-31 23:23:32 -0400

    - update

--- leetcode/src/main/java/leetcode/array/BoatsToSavePeople.java
+++ leetcode/src/main/java/leetcode/array/BoatsToSavePeople.java
@@ -35,5 +35,20 @@ public class BoatsToSavePeople {
             res++;
         }
         return res;
-    } 
+    }
+
+    public int numRescueBoats_3r(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0, right = people.length - 1;
+        int count = 0;
+        
+        while(left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            count++;
+        }
+        return count;
+    }
 }
