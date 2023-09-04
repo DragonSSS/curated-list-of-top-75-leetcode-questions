db59bc8c20c9eeb5df8b6642370c4f13e6bf700c
DragonSSS <songweilong1989@gmail.com>
2023-09-04 18:46:05 -0400

    - update

--- leetcode/src/main/java/leetcode/dfs/RangeSumOfBST.java
+++ leetcode/src/main/java/leetcode/dfs/RangeSumOfBST.java
@@ -17,4 +17,21 @@ public class RangeSumOfBST {
 
         return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
     }
+
+    // dfs
+    public int rangeSumBST_2r(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        }
+
+        if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+
+        return root.val + rangeSumBST(root.right, low, high) + rangeSumBST(root.left, low, high);
+    }
 }
