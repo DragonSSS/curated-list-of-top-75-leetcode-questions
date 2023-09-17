1b3f4b70fd0633819a3d27ebeec5b8a2fa730be3
DragonSSS <songweilong1989@gmail.com>
2023-09-17 15:28:16 -0400

    - update

--- leetcode/src/main/java/leetcode/heap/PathWithMinimumEffort.java
+++ leetcode/src/main/java/leetcode/heap/PathWithMinimumEffort.java
@@ -39,5 +39,44 @@ public class PathWithMinimumEffort {
             }
         }
         return -1;
-    } 
+    }
+
+    // int[][] dirs = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+    public int minimumEffortPath_2r(int[][] heights) {
+        // minHeap + int[][] globalMinEffor with update min value here;
+        //int {min_cost, i, j}
+        int m = heights.length, n = heights[0].length;
+        Integer[][] globalMinEffort = new Integer[m][n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        pq.offer(new int[]{0, 0, 0});
+        globalMinEffort[0][0] = 0;
+
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            int curMinCost = cur[0];
+            int x = cur[1];
+            int y = cur[2];
+
+            if (x == m - 1 && y == n - 1) {
+                return curMinCost;
+            }
+
+            for(int[] dir : dirs) {
+                int nextX = x + dir[0];
+                int nextY = y + dir[1];
+                if (nextX < 0 || nextX >= m || nextY < 0 || nextY >= n) {
+                    continue;
+                }
+
+                int nextCost = Math.abs(heights[nextX][nextY] - heights[x][y]);
+                // A route's effort is the maximum absolute difference in heights between two consecutive cells of the route.
+                int maxDiff = Math.max(curMinCost, nextCost);
+                if (globalMinEffort[nextX][nextY] == null || maxDiff < globalMinEffort[nextX][nextY]) {
+                    globalMinEffort[nextX][nextY] = maxDiff;
+                    pq.offer(new int[]{maxDiff, nextX, nextY});
+                }
+            }
+        }
+        return -1;
+    }
 }
